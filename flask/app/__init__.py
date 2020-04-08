3df94451b1a8720266a3eab12f49f8e5a2c47f59
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-04-08 13:18:27 -0400

    Flask: remove CSRF, add user loader

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,14 +2,12 @@ from flask import Flask, logging
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
 
 from app.config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 logging.create_logger(app)
-csrf = CSRFProtect(app)
 login = LoginManager(app)
 login.session_protection = 'strong'
 db = SQLAlchemy(app)
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -3,31 +3,31 @@ import json
 from flask import request
 from flask_login import login_user, logout_user, current_user, login_required
 
-from app import app, models
+from app import app, login, models
 
 
-@app.route('/')
-def index():
-    return json.dumps({})
+@login.user_loader
+def load_user(uid: int):
+    return models.User.query.get(uid)
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/api/login', methods=['POST'])
 def login():
     if current_user.is_authenticated:
         return '', 204
 
     body = request.json
     if not body:
-        return 'Request body must be JSON!', 400
+        return 'Request body must be correctly-shaped JSON!', 400
 
-    user = models.User.query.filter_by(username=body.username).first()
-    if user is None or not user.check_password(body.password):
+    user = models.User.query.filter_by(username=body['username']).first()
+    if user is None or not user.check_password(body['password']):
         return 'Unauthorized', 401
     login_user(user)
     return 'Authenticated', 200
 
 
-@app.route('/logout', methods=['POST'])
+@app.route('/api/logout', methods=['POST'])
 @login_required
 def logout():
     if not request.json:
