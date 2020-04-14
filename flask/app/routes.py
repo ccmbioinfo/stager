77541fe286ac332a7e0067460ccad380ce454e20
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-04-14 15:24:13 -0400

    Flask: port admin endpoints from old Sample Tracker, excluding permission-level check

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,5 +1,7 @@
 import json
 
+from functools import wraps
+
 from flask import request
 from flask_login import login_user, logout_user, current_user, login_required
 
@@ -45,3 +47,106 @@ def logout():
         return 'Request body must be JSON!', 400
     logout_user()
     return '', 204
+
+
+def check_admin(handler):
+    @wraps(handler)
+    def decorated_handler(*args, **kwargs):
+        if False:
+            return 'Unauthorized', 401
+        return handler(*args, **kwargs)
+
+    return decorated_handler
+
+
+def validate_user(request_user: dict):
+    if 'username' in request_user:
+        if 'password' in request_user and len(request_user['password']):
+            return 'confirmPassword' in request_user and request_user['password'] == request_user['confirmPassword']
+        return True
+    return False
+
+
+@app.route('/api/users', methods=['GET'])
+@login_required
+@check_admin
+def user_list():
+    db_users = db.session.query(models.User).all()
+    users = [
+        {
+            'username': user.username,
+            'email': user.email,
+            'isAdmin': True
+        }
+        for user in db_users
+    ]
+    return json.dumps(users)
+
+
+@app.route('/api/users', methods=['POST'])
+@login_required
+@check_admin
+def create_user():
+    rq_user = request.get_json()
+    if not validate_user(rq_user):
+        return 'Bad request', 400
+
+    db_user = models.User.query.filter_by(username=rq_user['username']).first()
+    if db_user is not None:
+        return 'User already exists', 403
+
+    if 'password' not in rq_user or 'email' not in rq_user:
+        return 'Bad request', 400
+
+    user = models.User(
+        username=rq_user['username'],
+        email=rq_user['email']
+    )
+    user.set_password(rq_user['password'])
+    db.session.add(user)
+    try:
+        db.session.commit()
+        return 'Created', 201
+    except:
+        db.session.rollback()
+        return 'Server error', 500
+
+
+@app.route('/api/users', methods=['PUT'])
+@login_required
+@check_admin
+def update_user():
+    rq_user = request.get_json()
+    if not validate_user(rq_user):
+        return 'Bad request', 400
+
+    db_user = models.User.query.filter_by(username=rq_user['username']).first_or_404()
+    if 'password' in rq_user and len(rq_user['password']):
+        db_user.set_password(rq_user['password'])
+    if 'email' in rq_user:
+        db_user.email = rq_user['email']
+
+    try:
+        db.session.commit()
+        return 'Updated', 204
+    except:
+        db.session.rollback()
+        return 'Server error', 500
+
+
+@app.route('/api/users', methods=['DELETE'])
+@login_required
+@check_admin
+def delete_user():
+    rq_user = request.get_json()
+    if not validate_user(rq_user):
+        return 'Bad request', 400
+
+    db_user = models.User.query.filter_by(username=rq_user['username']).first_or_404()
+    try:
+        db.session.delete(db_user)
+        db.session.commit()
+        return 'Updated', 204
+    except:
+        db.session.rollback()
+        return 'Server error', 500
