940e70adf5e79935c146b534428f4e2794bbc69a
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-06-23 18:34:16 -0400

    React: redo login form with Material Design and MUI

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,17 +1,18 @@
 import React, {useState} from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
-import LoginForm from './login/Login';
 import UserList from './admin/UserList';
 
+import LoginForm from './pages/Login';
 import Navigation from './pages/Navigation';
 
 export default function App() {
     const [authenticated, setAuthenticated] = useState(false);
-    return (
-        <Navigation />
-    );
+    if (authenticated) {
+        return <LoginForm setAuthenticated={setAuthenticated} />;
+    } else {
+        return <Navigation />;
+    }
 }
