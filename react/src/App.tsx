32d43e6b600ac0f099cc3182ca7b55a6543685d4
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-06-25 15:36:38 -0400

    React: check authentication at startup without flashing the sign-in prompt

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import LoginForm from './pages/Login';
 import Navigation from './pages/Navigation';
 
 export default function App() {
-    const [authenticated, setAuthenticated] = useState(false);
+    const [authenticated, setAuthenticated] = useState<boolean | null>(null);
     async function signout() {
         const result = await fetch("/api/logout", {
             method: "POST",
@@ -15,7 +15,13 @@ export default function App() {
             setAuthenticated(false);
         }
     }
-    if (authenticated) {
+    // Check if already signed in
+    useEffect(() => {
+        fetch("/api/login", { method: "POST" }).then(result => setAuthenticated(result.ok));
+    }, []);
+    if (authenticated === null) {
+        return <></>;
+    } else if (authenticated) {
         return <Navigation signout={signout} />;
     } else {
         return <LoginForm setAuthenticated={setAuthenticated} />;
