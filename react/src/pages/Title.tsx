4ea585fa9dd5a45a25d09ed98de76c613b506581
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-06-26 16:51:48 -0400

    React: format dashboard-related files and convert to TypeScript (#7)
    * React: format Dashboard
    
    * React: convert dashboard tables to TypeScript and reformat
    
    * React: convert Title to TypeScript

---
+++ react/src/pages/Title.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export default function Title(props: { children: React.ReactNode }) {
+    return (
+        <Typography variant="button" display="block" gutterBottom color="primary">
+            {props.children}
+        </Typography>
+    );
+}
--- react/src/pages/dashboard/Dashboard.tsx
+++ react/src/pages/dashboard/Dashboard.tsx
@@ -7,54 +7,54 @@ import AnalysisTable from './AnalysisTable';
 import ParticipantTable from './ParticipantTable';
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'fill',
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
+    root: {
+        display: 'fill',
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
 }));
 
 export default function Dashboard() {
-  const classes = useStyles();
-  useEffect(() => {
-    document.title = "Dashboard | ST2020";
-  }, []);
+    const classes = useStyles();
+    useEffect(() => {
+        document.title = "Dashboard | ST2020";
+    }, []);
 
-  return (
-    <main className={classes.content}>
-      <div className={classes.appBarSpacer} />
-      <Container maxWidth="lg" className={classes.container}>
-        <Grid container spacing={0}>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <AnalysisTable />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-      <Container>
-        <Grid container spacing={0}>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <ParticipantTable />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    </main>
-  );
+    return (
+        <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Container maxWidth="lg" className={classes.container}>
+                <Grid container spacing={0}>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <AnalysisTable />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Container>
+                <Grid container spacing={0}>
+                    <Grid item xs={12}>
+                        <Paper className={classes.paper}>
+                            <ParticipantTable />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </main>
+    );
 }
