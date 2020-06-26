1aa8d02bda107c30bfead3c0ced784d426e49e9a
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-06-26 17:02:22 -0400

    React: reformat participants

--- react/src/pages/participants/Card.tsx
+++ react/src/pages/participants/Card.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/styles';
-import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import TrendingUp from '@material-ui/icons/TrendingUp';
 
 export interface CardProps {
@@ -14,47 +14,47 @@ export interface CardProps {
 }
 
 function preventDefault(event: any) {
-  event.preventDefault();
+    event.preventDefault();
 }
 
-const useStyles = makeStyles((theme) => ({
-  depositContext: {
-    flex: 1,
-  },
-  icon: {
-    height: 24,
-    width: 24,
-    color: 'green',
-  },
+const useStyles = makeStyles(theme => ({
+    depositContext: {
+        flex: 1,
+    },
+    icon: {
+        height: 24,
+        width: 24,
+        color: 'green',
+    },
 }));
 
-export default function Card({title, value, textSecondary, linkText, children}: CardProps) {
-  const classes = useStyles();
+export default function Card({ title, value, textSecondary, linkText, children }: CardProps) {
+    const classes = useStyles();
 
-  return (
-    <React.Fragment>
-      <Typography variant="h5" color="primary" gutterBottom>
-        {title}
-      </Typography>
-      <Typography component="p" variant="h4">
-        {value}
-      </Typography>
-      <Grid container>
-      <Grid item xs={1} >
-        <TrendingUp className={classes.icon} />
-      </Grid>
-      <Grid item xs={11}>
-        <Typography color="textSecondary" gutterBottom className={classes.depositContext}>
-          {textSecondary}
-        </Typography>
-      </Grid>
-      </Grid>
-      {children}
-      <div>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          {linkText}
-        </Link>
-      </div>
-    </React.Fragment>
-  );
+    return (
+        <React.Fragment>
+            <Typography variant="h5" color="primary" gutterBottom>
+                {title}
+            </Typography>
+            <Typography component="p" variant="h4">
+                {value}
+            </Typography>
+            <Grid container>
+                <Grid item xs={1} >
+                    <TrendingUp className={classes.icon} />
+                </Grid>
+                <Grid item xs={11}>
+                    <Typography color="textSecondary" gutterBottom className={classes.depositContext}>
+                        {textSecondary}
+                    </Typography>
+                </Grid>
+            </Grid>
+            {children}
+            <div>
+                <Link color="primary" href="#" onClick={preventDefault}>
+                    {linkText}
+                </Link>
+            </div>
+        </React.Fragment>
+    );
 }
--- react/src/pages/participants/Participants.tsx
+++ react/src/pages/participants/Participants.tsx
@@ -1,75 +1,75 @@
 import React, { useEffect } from 'react';
-import Card from './Card';
-
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+
+import Card from './Card';
 import ParticipantsTable, { DisplayType } from './ParticipantsTable';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    display: 'fill',
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
-  },
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'fill',
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
 }));
 
 export default function Participants() {
-  const classes = useStyles();
-  const [display, setDisplay] = React.useState(DisplayType.PARTICIPANT);
+    const classes = useStyles();
+    const [display, setDisplay] = React.useState(DisplayType.PARTICIPANT);
 
-  useEffect(() => {
-    document.title = "Participants | ST2020";
-  }, []);
+    useEffect(() => {
+        document.title = "Participants | ST2020";
+    }, []);
 
-  return (
-    <main className={classes.content}>
-      <div className={classes.appBarSpacer} />
-      <Container maxWidth="lg" className={classes.container}>
-        <Grid container spacing={2}>
-          <Grid item xs={4}>
-            <Paper className={classes.paper}>
-              <Card title={"Participants"} value={"2731"} textSecondary={"12 since last week"} linkText={"Participant View"}>
-              </Card>
-            </Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className={classes.paper}>
-            <Card title={"Datasets"} value={"3012"} textSecondary={"3 CES, 5 RES since last week"} linkText={"Dataset View"} children=""/>
-            </Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className={classes.paper}>
-              <Card title={"Families"} value={"728"} textSecondary={"2 since last week"} linkText={"Family View"} children=""/>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    <Container >
-      <Grid container spacing={0}>
-        <Grid item xs={12}>
-          <ParticipantsTable display={display}/>
-        </Grid>
-      </Grid>
-    </Container>
-    </main>
-  );
+    return (
+        <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Container maxWidth="lg" className={classes.container}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <Paper className={classes.paper}>
+                            <Card title="Participants" value="2731" textSecondary="12 since last week" linkText="Participant View">
+                            </Card>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={classes.paper}>
+                            <Card title="Datasets" value="3012" textSecondary="3 CES, 5 RES since last week" linkText="Dataset View" children="" />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={classes.paper}>
+                            <Card title="Families" value="728" textSecondary="2 since last week" linkText="Family View" children="" />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Container >
+                <Grid container spacing={0}>
+                    <Grid item xs={12}>
+                        <ParticipantsTable display={display} />
+                    </Grid>
+                </Grid>
+            </Container>
+        </main>
+    );
 }
