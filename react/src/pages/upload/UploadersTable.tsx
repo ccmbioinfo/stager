5fc6e05cb1885b54df2700bbac91e619ca66dbf1
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-06-26 17:11:03 -0400

    React: format settings and upload

--- react/src/pages/upload/UploadersTable.tsx
+++ react/src/pages/upload/UploadersTable.tsx
@@ -1,28 +1,29 @@
 import React from 'react'
-import MaterialTable, { MTableToolbar } from 'material-table';
+import { makeStyles } from '@material-ui/core/styles';
 import Chip from '@material-ui/core/Chip';
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import MaterialTable, { MTableToolbar } from 'material-table';
+
 import HidableTextButton from './HidableTextButton';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  chip: {
-    color: "primary",
-    marginRight: '10px',
-    colorPrimary: theme.palette.primary,
-  },
-  button: {
-    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-    border: 0,
-    borderRadius: 3,
-    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-    color: 'white',
-    height: 48,
-    padding: '0 30px',
-  },
+const useStyles = makeStyles(theme => ({
+    chip: {
+        color: "primary",
+        marginRight: '10px',
+        colorPrimary: theme.palette.primary,
+    },
+    button: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+    },
 }));
 
 function createFile(name: string, filesUnlinked: number, filesUploaded: number, lastActive: string, secretKey: string) {
-    return {name, filesUnlinked, filesUploaded, lastActive, secretKey};
+    return { name, filesUnlinked, filesUploaded, lastActive, secretKey };
 }
 
 const rows = [
@@ -37,34 +38,34 @@ export default function FilesTable() {
     const classes = useStyles();
 
     return (
-      <MaterialTable
-      columns={[
-        { title: 'Organization', field: 'name' },
-        { title: 'Files unlinked', field: 'filesUnlinked', type: 'numeric' },
-        { title: 'Files uploaded', field: 'filesUploaded', type: 'string' },
-        { title: 'Last Active', field: 'lastActive', type: 'string' },
-        { title: 'Secret Key', field: 'secretKey', type: 'string', render: rowData => <HidableTextButton secretText={rowData.secretKey}/> }
-      ]}
-      data={rows}
-      title={"Uploaders"}
-      options={{
-        pageSize : 10,
-        selection: true
-      }}
-      components={{
-        Toolbar: props => (
-          <div>
-            <MTableToolbar {...props} />
-            <div style={{marginLeft: '24px'}}>
-              <Chip label="CHEO" clickable className={classes.chip}/>
-              <Chip label="SK" clickable className={classes.chip}/>
-              <Chip label="ACH" clickable className={classes.chip}/>
-              <Chip label="BCL" clickable className={classes.chip}/>
-              <Chip label="Misc." clickable className={classes.chip}/>
-            </div>
-          </div>
-        ),
-      }}
-    />
+        <MaterialTable
+            columns={[
+                { title: 'Organization', field: 'name' },
+                { title: 'Files unlinked', field: 'filesUnlinked', type: 'numeric' },
+                { title: 'Files uploaded', field: 'filesUploaded', type: 'string' },
+                { title: 'Last Active', field: 'lastActive', type: 'string' },
+                { title: 'Secret Key', field: 'secretKey', type: 'string', render: rowData => <HidableTextButton secretText={rowData.secretKey} /> }
+            ]}
+            data={rows}
+            title="Uploaders"
+            options={{
+                pageSize: 10,
+                selection: true
+            }}
+            components={{
+                Toolbar: props => (
+                    <div>
+                        <MTableToolbar {...props} />
+                        <div style={{ marginLeft: '24px' }}>
+                            <Chip label="CHEO" clickable className={classes.chip} />
+                            <Chip label="SK" clickable className={classes.chip} />
+                            <Chip label="ACH" clickable className={classes.chip} />
+                            <Chip label="BCL" clickable className={classes.chip} />
+                            <Chip label="Misc." clickable className={classes.chip} />
+                        </div>
+                    </div>
+                ),
+            }}
+        />
     )
-  }
+}
--- react/src/pages/upload/Uploads.tsx
+++ react/src/pages/upload/Uploads.tsx
@@ -1,93 +1,94 @@
-import React, { useEffect } from 'react';
-import { makeStyles, Theme } from '@material-ui/core/styles';
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
+
 import Card from '../participants/Card';
-import Box from '@material-ui/core/Box';
 import FilesTable from './FilesTable';
 import UploadersTable from './UploadersTable';
-import Link from '@material-ui/core/Link';
 
 enum TableType {
-  FILES,
-  UPLOADERS,
+    FILES,
+    UPLOADERS,
 }
 
-const useStyles = makeStyles((theme: Theme) => ({
-  root: {
-    display: 'fill',
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(3),
-    paddingBottom: theme.spacing(3),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
-  },
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'fill',
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        height: '100vh',
+        overflow: 'auto',
+    },
+    container: {
+        paddingTop: theme.spacing(3),
+        paddingBottom: theme.spacing(3),
+    },
+    paper: {
+        padding: theme.spacing(2),
+        display: 'flex',
+        overflow: 'auto',
+        flexDirection: 'column',
+    },
+    fixedHeight: {
+        height: 240,
+    },
 }));
 
 function renderTable(displayTable: TableType) {
-  switch(displayTable) {
-    case TableType.FILES:
-      return <FilesTable />;
-    case TableType.UPLOADERS:
-      return <UploadersTable />;
-  }
+    switch (displayTable) {
+        case TableType.FILES:
+            return <FilesTable />;
+        case TableType.UPLOADERS:
+            return <UploadersTable />;
+    }
 }
 
 
 export default function Uploads() {
-  const classes = useStyles();
-  const [tableType, setTableType] = React.useState(TableType.FILES);
+    const classes = useStyles();
+    const [tableType, setTableType] = useState(TableType.FILES);
 
-  useEffect(() => {
-    document.title = "Upload | ST2020";
-  }, []);
+    useEffect(() => {
+        document.title = "Upload | ST2020";
+    }, []);
 
-  return (
-    <main className={classes.content}>
-      <div className={classes.appBarSpacer} />
-      <Container maxWidth="lg" className={classes.container}>
-       <Grid container spacing={2}>
-       <Grid item xs={4}>
-            <Paper className={classes.paper}>
-              <Card title={"Unlinked files"} value={"17"} textSecondary={"2 since last week"} linkText={""} children={
-                <Link color="primary" href="#" onClick={() => {setTableType(TableType.FILES); console.log(tableType)}}>
-                  View unlinked files
-                </Link>
-              }/>
-            </Paper>
-          </Grid>
-          <Grid item xs={4}>
-            <Paper className={classes.paper}>
-              <Card title={"Uploaders"} value={"5"} textSecondary={"Avg 12 files per week"} linkText={""} children={
-                <Link color="primary" href="#" onClick={() => {setTableType(TableType.UPLOADERS); console.log(tableType)}}>
-                  Manage uploaders
-                </Link>
-              }/>
-            </Paper>
-          </Grid>
-          <Grid item xs={12}>
-            <Box alignItems='stretch'>
-              {renderTable(tableType)}
-            </Box>
-        </Grid>
-       </Grid>
-      </Container>
-    </main>
+    return (
+        <main className={classes.content}>
+            <div className={classes.appBarSpacer} />
+            <Container maxWidth="lg" className={classes.container}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <Paper className={classes.paper}>
+                            <Card title="Unlinked files" value="17" textSecondary="2 since last week" linkText="" children={
+                                <Link color="primary" href="#" onClick={() => { setTableType(TableType.FILES); console.log(tableType) }}>
+                                    View unlinked files
+                                </Link>
+                            } />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Paper className={classes.paper}>
+                            <Card title="Uploaders" value="5" textSecondary="Avg 12 files per week" linkText="" children={
+                                <Link color="primary" href="#" onClick={() => { setTableType(TableType.UPLOADERS); console.log(tableType) }}>
+                                    Manage uploaders
+                                </Link>
+                            } />
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Box alignItems="stretch">
+                            {renderTable(tableType)}
+                        </Box>
+                    </Grid>
+                </Grid>
+            </Container>
+        </main>
 
-  );
+    );
 }
