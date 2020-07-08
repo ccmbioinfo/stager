c58b3effe22b5a21b50beec1580753a7db330dd3
Hillary Elrick <hillary.elrick@sickkids.ca>
2020-07-08 10:17:27 -0400

    updating samples table

--- react/src/pages/dashboard/SamplesTable.tsx
+++ react/src/pages/dashboard/SamplesTable.tsx
@@ -4,7 +4,7 @@ import Chip from '@material-ui/core/Chip';
 import MaterialTable, { MTableToolbar } from 'material-table';
 
 
-export interface Participant {
+export interface Sample {
     participantID: string,
     project: string,
     uploader: string,
@@ -18,13 +18,14 @@ export interface Participant {
 
 const useStyles = makeStyles(theme => ({
     chip: {
-        color: "primary",
         marginRight: '10px',
-        colorPrimary: theme.palette.primary,
+        '&.Mui-click': {
+            backgroundColor: '#f50057',
+        }
     }
 }));
 
-function createParticipant(
+function createSample(
     participantID: string,
     project: string,
     uploader: string,
@@ -38,19 +39,23 @@ function createParticipant(
     return { participantID, project, uploader, tissueType, dataType, relation, numSamples, sex, uploaded };
 }
 
-const rows: Participant[] = [
-    createParticipant('AA0001', '3001', 'CHEO', 'Blood', 'WES', 'Proband', 2, 'F', '2020-02-01'),
-    createParticipant('AA0002', '3001', 'CHEO', 'Blood', 'WES', 'Mother', 1, 'F', '2020-02-01'),
-    createParticipant('AA0003', '3001', 'CHEO', 'Blood', 'WES', 'Father', 1, 'M', '2020-02-01'),
-    createParticipant('BB0001', '2001', 'SK', 'Muscle', 'RNASeq', 'Proband', 1, 'F', '2020-03-11'),
-    createParticipant('BB0002', '2002', 'BCL', 'Blood', 'WGS', 'Proband', 1, 'M', '2020-03-11'),
-    createParticipant('CC0003', '2003', 'CHEO', 'Blood', 'WES', 'Proband', 1, 'F', '2020-03-11'),
-    createParticipant('CC0004', '2003', 'CHEO', 'Blood', 'WES', 'Father', 1, 'M', '2020-05-23'),
-    createParticipant('AA0005', '3012', 'SK', 'Saliva', 'WES', 'Proband', 1, 'M', '2020-05-23'),
+const rows: Sample[] = [
+    createSample('AA0001', '3001', 'CHEO', 'Blood', 'WES', 'Proband', 2, 'F', '2020-02-01'),
+    createSample('AA0002', '3001', 'CHEO', 'Blood', 'WES', 'Mother', 1, 'F', '2020-02-01'),
+    createSample('AA0003', '3001', 'CHEO', 'Blood', 'WES', 'Father', 1, 'M', '2020-02-01'),
+    createSample('BB0001', '2001', 'SK', 'Muscle', 'RNASeq', 'Proband', 1, 'F', '2020-03-11'),
+    createSample('BB0002', '2002', 'BCL', 'Blood', 'WGS', 'Proband', 1, 'M', '2020-03-11'),
+    createSample('CC0003', '2003', 'CHEO', 'Blood', 'WES', 'Proband', 1, 'F', '2020-03-11'),
+    createSample('CC0004', '2003', 'CHEO', 'Blood', 'WES', 'Father', 1, 'M', '2020-05-23'),
+    createSample('AA0005', '3012', 'SK', 'Saliva', 'WES', 'Proband', 1, 'M', '2020-05-23'),
+    createSample('AA0047', '3001', 'ACH', 'Blood', 'WES', 'Proband', 2, 'M', '2020-02-01'),
+    createSample('AA0049', '3001', 'ACH', 'Blood', 'WES', 'Sibling', 1, 'F', '2020-02-01'),
+    createSample('AA0048', '3001', 'ACH', 'Blood', 'WES', 'Father', 1, 'M', '2020-02-01'),
 ];
 
 export default function SamplesTable() {
     const classes = useStyles();
+    const [centre, setCentre] = useState("");
 
     return (
         <div>
@@ -58,7 +63,7 @@ export default function SamplesTable() {
                 columns={[
                     { title: 'Project', field: 'project' },
                     { title: 'Participant', field: 'participantID' },
-                    { title: 'Uploader', field: 'uploader' },
+                    { title: 'Uploader', field: 'uploader' , defaultFilter: centre},
                     { title: 'Tissue', field: 'tissueType'},
                     { title: 'Data Type', field: 'dataType'},
                     { title: 'Relation', field: 'relation'},
@@ -66,7 +71,7 @@ export default function SamplesTable() {
                     { title: 'Uploaded', field: 'uploaded', type: 'string' }
                 ]}
                 data={rows}
-                title='Available Samples'
+                title='3012 Available Samples'
                 options={{
                     pageSize: 10,
                     selection: false,
@@ -77,10 +82,11 @@ export default function SamplesTable() {
                         <div>
                             <MTableToolbar {...props} />
                             <div style={{ marginLeft: '24px' }}>
-                                <Chip label="CHEO" clickable className={classes.chip} />
-                                <Chip label="SK" clickable className={classes.chip} />
-                                <Chip label="ACH" clickable className={classes.chip} />
-                                <Chip label="BCL" clickable className={classes.chip} />
+                                <Chip label="CHEO" clickable className={classes.chip} onClick={() => setCentre("CHEO")}/>
+                                <Chip label="SK" clickable className={classes.chip} onClick={() => setCentre("SK")}/>
+                                <Chip label="ACH" clickable className={classes.chip} onClick={() => setCentre("ACH")}/>
+                                <Chip label="BCL" clickable className={classes.chip} onClick={() => setCentre("BCL")}/>
+                                <Chip label="x" clickable className={classes.chip} onClick={() => setCentre("")}/>
                             </div>
                         </div>
                     ),
