f3a4be76762429752082e9a91a398cd634465d92
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-09-23 09:58:30 -0400

    Flask: log queries via SQLAlchemy to console

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,13 +1,18 @@
-from flask import Flask, logging
+import logging
+from flask import Flask, logging as flask_logging
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 from app.config import Config
 
+logging.basicConfig()
+logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+logging.getLogger('sqlalchemy.pool').setLevel(logging.INFO)
+
 app = Flask(__name__)
 app.config.from_object(Config)
-logging.create_logger(app)
+flask_logging.create_logger(app)
 login = LoginManager(app)
 login.session_protection = 'strong'
 db = SQLAlchemy(app)
