6cfca6e50d4e6bf2daad92372aef1fb1abffecc7
Mackenzie Frew <11793063+frewmack@users.noreply.github.com>
2020-10-01 11:58:46 -0400

    Add placeholder for auto-refreshing data

--- react/src/pages/analysis/Analysis.tsx
+++ react/src/pages/analysis/Analysis.tsx
@@ -165,6 +165,10 @@ function rowsToString(rows: AnalysisRow[], delim?: string) {
     return returnStr;
 }
 
+// How long to wait before refreshing data, in milliseconds
+// Default: 1 min
+const refreshTimeDelay = 1000 * 60;
+
 export default function Analysis() {
     const classes = useStyles();
     const { analysisID }= useParams<ParamTypes>();
@@ -182,6 +186,12 @@ export default function Analysis() {
 
         // TODO: Fetch data here
         setRows(analyses);
+
+        // Auto-refresh
+        // const dataRefreshTimer = setInterval(() => {
+        //     // TODO: Fetch data here, set rows
+            
+        // }, refreshTimeDelay)
 
     }, []);
 
