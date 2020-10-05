458a35abc4f90863bf5ec9766ad9f91dad05b070
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-10-05 16:22:17 -0400

    Update AddAnalysisAlert to reference Datasets page

--- react/src/pages/analysis/AddAnalysisAlert.tsx
+++ react/src/pages/analysis/AddAnalysisAlert.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, ButtonGroup,  Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@material-ui/core';
-import { useHistory } from 'react-router';
 
 interface AddAnalysisAlertProps {
     open: boolean,
@@ -19,16 +18,16 @@ export default function AddAnalysisAlert(props: AddAnalysisAlertProps) {
             <DialogTitle id="alert-dialog-title">Request Analysis</DialogTitle>
             <DialogContent>
             <DialogContentText id="alert-dialog-description">
-                New analyses are created in the Participants panel. Would you like to be moved to the Participants panel?
+                Analyses are requested from the Datasets page. Would you like to go there now?
             </DialogContentText>
             </DialogContent>
             <DialogActions>
             <ButtonGroup>
                 <Button onClick={props.onClose} color="default" variant="contained">
-                    Decline
+                    No
                 </Button>
                 <Button onClick={props.onAccept} color="primary" autoFocus variant="contained">
-                    Accept
+                    Yes
                 </Button>
             </ButtonGroup>
             </DialogActions>
