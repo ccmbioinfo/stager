d8375ca0e6d8c3b570e77eb5192d0bf12fa4dd9b
Madeline Couse <mhcouse@gmail.com>
2020-10-05 10:54:38 -0400

    add last_login to dummy user table

--- flask/app/manage.py
+++ flask/app/manage.py
@@ -1,11 +1,13 @@
 from app import app, db, models
+from datetime import datetime
 
 @app.cli.command('add-default-admin')
 def add_default_admin():
     if len(db.session.query(models.User).all()) == 0:
         default_admin = models.User(
             username=app.config.get('DEFAULT_ADMIN'),
-            email=app.config.get('DEFAULT_ADMIN_EMAIL')
+            email=app.config.get('DEFAULT_ADMIN_EMAIL'),
+            last_login= datetime.now()
         )
         default_admin.set_password(app.config.get('DEFAULT_PASSWORD'))
         db.session.add(default_admin)
