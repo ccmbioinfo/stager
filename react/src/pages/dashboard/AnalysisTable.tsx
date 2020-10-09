2fb9c875194e40d9bc5f9980010aaadec0083f6c
annnag <40178719+annnag@users.noreply.github.com>
2020-10-09 13:14:41 -0700

    React: add participant detail modal on clicking dashboard rows (#69)

---
+++ react/src/pages/dashboard/AnalysisTable.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import MaterialTable from 'material-table';
+import { getAnalyses } from './MockData';
+
+interface ParticipantInfoProp {
+    participantID: string
+}
+
+export default function AnalysisTable({ participantID }: ParticipantInfoProp) {
+
+    //fetch participant analyses
+    const analyses = getAnalyses(participantID);
+
+    return (
+        <MaterialTable
+            columns={[
+                { title: 'Analysis ID', field: 'analysisID' },
+                { title: 'Dataset ID', field: 'datasetID' },
+                { title: 'Analysis State', field: 'analysisState' },
+                { title: 'Pipeline ID', field: 'pipelineID' },
+                { title: 'Qsub ID', field: 'qsubID' },
+                { title: 'Result HPF Path', field: 'resultHpfPath' },
+                { title: 'Assignee', field: 'assignee' },
+                { title: 'Requester', field: 'requester' },
+                { title: 'Requested', field: 'requested' },
+                { title: 'Started', field: 'started' },
+                { title: 'Finished', field: 'finished' },
+                { title: 'Notes', field: 'notes' },
+                { title: 'Updated', field: 'updated' },
+                { title: 'Updated By', field: 'updatedBy' },
+            ]}
+            data={analyses}
+            title="Analyses"
+            options={{
+                paging: false,
+                selection: false,
+                search: false,
+            }}
+        />
+    );
+}
