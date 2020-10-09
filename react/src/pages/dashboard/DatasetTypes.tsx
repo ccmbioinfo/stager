6ef4a1d122974994fa19e6df7acd29e6d6f02b7f
Anna Guo <annaguo8010@gmail.com>
2020-10-09 09:18:37 -0400

    refactor dataset type chips

--- react/src/pages/dashboard/DatasetTypes.tsx
+++ react/src/pages/dashboard/DatasetTypes.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import DatasetType from './DatasetType';
+import { makeStyles, Chip, Avatar } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    chip: {
+        margin: theme.spacing(.5)
+    },
+}));
 
 interface DatasetTypesProps {
     datasetTypes: {[key: string]: number},
 }
 
 export default function DatasetTypes({ datasetTypes }: DatasetTypesProps) {
+    const classes = useStyles();
+
+    const getChip = (type: string, number: number) => {
+        return <Chip className={classes.chip} label={type} avatar={<Avatar>{number}</Avatar>} />
+    } 
+
     return (
         <div>
-            {Object.keys(datasetTypes).map(type => <DatasetType type={type} number={datasetTypes[type]} />)}
+            {Object.keys(datasetTypes).map(type => getChip(type, datasetTypes[type]))}
         </div>
-    )
+    );
 }
