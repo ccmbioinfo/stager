80384362462517c30d0c9d5f5bf2cf594d3abb0d
Mackenzie Frew <11793063+frewmack@users.noreply.github.com>
2020-10-09 14:54:51 -0400

    Separate into smaller test cases

--- flask/tests/test_login.py
+++ flask/tests/test_login.py
@@ -4,6 +4,21 @@ from flask import request, jsonify, current_app as app
 
 # Helper functions
 
+USERNAME = "testuser"
+EMAIL = "testuser@example.com"
+PASSWORD = "password"
+
+def add_test_user():
+    user = models.User(
+        username=USERNAME,
+        email=EMAIL
+    )
+    user.set_password(PASSWORD)
+
+    db.session.add(user)
+    db.session.commit()
+
+
 def login(client, username, password):
     """ Attempt to login using the provided credentials with the client. """
     return client.post('/api/login', json={
@@ -19,18 +34,8 @@ def logout(client):
 # Test cases
 
 def test_login_logout(client):
-    USERNAME = "testuser"
-    EMAIL = "testuser@example.com"
-    PASSWORD = "password"
-    user = models.User(
-        username=USERNAME,
-        email=EMAIL
-    )
-    user.set_password(PASSWORD)
-
-    db.session.add(user)
-    db.session.commit()
-
+    """ Tests logging in and out. Also tests last_login. """
+    add_test_user()
 
     # Logging in with correct credentials
     res = login(client, USERNAME, PASSWORD)
@@ -49,6 +54,22 @@ def test_login_logout(client):
     assert res.data == b''
     # assert not user.is_authenticated
 
+    # Logging in a second time
+    res = login(client, USERNAME, PASSWORD)
+
+    assert res.status_code == 200
+
+    data = res.get_json()
+
+    assert data['username'] == USERNAME
+    assert data['last_login'] is not None # Second time logging in
+    # assert user.is_authenticated
+
+
+def test_wrong_credentials(client):
+    """ Tests response to invalid login credentials. """
+    add_test_user()
+
     # Logging in with wrong username
     res = login(client, USERNAME + "x", PASSWORD)
 
@@ -61,10 +82,4 @@ def test_login_logout(client):
     assert res.status_code == 401
     assert res.data == b'Unauthorized'
 
-    # Logging in a second time
-    res = login(client, USERNAME, PASSWORD)
-    data = res.get_json()
-
-    assert data['username'] == USERNAME
-    assert data['last_login'] is not None
 
