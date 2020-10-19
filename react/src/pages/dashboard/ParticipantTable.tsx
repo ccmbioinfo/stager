3d9659c272c7930ce39db3ea4d062ed13c88949e
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-10-19 12:57:23 -0400

    Remove Typography rendering for dashboard notes

--- react/src/pages/dashboard/ParticipantTable.tsx
+++ react/src/pages/dashboard/ParticipantTable.tsx
@@ -45,7 +45,7 @@ export default function ParticipantTable() {
                     samples.forEach((sample: Sample) => {
                         const datasets = sample.datasets;
                         datasets.forEach((dataset: Dataset) => {
-                            participant['dataset_types'] ? 
+                            participant['dataset_types'] ?
                             participant['dataset_types'].push(dataset.dataset_type) :
                             participant['dataset_types'] = [dataset.dataset_type];
                         })
@@ -74,7 +74,7 @@ export default function ParticipantTable() {
                     { title: 'Affected', field: 'affected', type: 'boolean', align: 'center'},
                     { title: 'Solved', field: 'solved', type: 'boolean', align: 'center'},
                     { title: 'Sex', field: 'sex', type: 'string', align: 'center', lookup: sexTypes},
-                    { title: 'Notes', field: 'notes', width: "50%", render: (rowData) => <Typography>{ rowData.notes }</Typography>},
+                    { title: 'Notes', field: 'notes', width: "50%" },
                     { title: 'Dataset Types', field: 'dataset_types', align: 'center', lookup: datasetTypes, render: (rowData) => <DatasetTypes datasetTypes={countArray(rowData.dataset_types)} />}
                 ]}
                 data={participants}
