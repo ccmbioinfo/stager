937a0a9b154c97f34ee556dbc7a6bd8e69106b67
Anna Guo <annaguo8010@gmail.com>
2020-10-22 11:45:56 -0400

    fix warning

--- react/src/pages/utils.tsx
+++ react/src/pages/utils.tsx
@@ -20,71 +20,13 @@ const months = [
 export type Counts = { [key: string]: number };
 export type KeyValue = { [key: string]: string };
 
-/*****   FUNCTIONS   *****/
-export function countArray(items: string[]) {
-    return items.reduce<Counts>((counts, item) => {
-        if (counts[item]) {
-            counts[item] += 1;
-        } else {
-            counts[item] = 1;
-        }
-        return counts;
-    }, Object.create(null));
-}
-
-export function toKeyValue(items: string[]) {
-    return items.reduce<KeyValue>((map, item) => {
-        map[item] = item;
-        return map;
-    }, Object.create(null));
-}
-
-/**
- * Return a date string in the format "YYYY-MM-DD", if possible.
- *
- * @param {string} date Datestring of the form: "Day, DD Mon YYYY HH:MM:SS GMT"
- */
-export function formatDateString(date: string) {
-    // Pretty general datestring because we trust the server to send a good one
-    const regex = /^[A-Z][a-z]{2}, (\d{2}) ([A-Z][a-z]{2}) (\d{4}) \d{2}:\d{2}:\d{2} GMT$/
-    const result = regex.exec(date);
-    if (result) {
-        let [year, month, day] = [result[3], '' + (months.indexOf(result[2]) + 1), result[1]];
-        if (month.length < 2)
-            month = '0' + month;
-        return [year, month, day].join('-');
-    }
-    return date;
-}
-
-/**
- * Convert the provided JSON Array to a valid array of Analysis.
- */
-export function jsonToAnalyses(data: Array<any>): Analysis[] {
-    const rows: Analysis[] = data.map((row, index, arr) => {
-        switch (row.analysis_state) {
-            case 'Requested':
-                row.state = PipelineStatus.PENDING;
-                break;
-            case 'Running':
-                row.state = PipelineStatus.RUNNING;
-                break;
-            case 'Done':
-                row.state = PipelineStatus.COMPLETED;
-                break;
-            case 'Error':
-                row.state = PipelineStatus.ERROR;
-                break;
-            case 'Cancelled':
-                row.state = PipelineStatus.CANCELLED;
-                break;
-            default:
-                row.state = null;
-                break;
-        }
-        return { ...row, selected: false } as Analysis;
-    });
-    return rows;
+/*****   ENUMS   *****/
+export enum PipelineStatus {
+    PENDING = "Pending",
+    RUNNING = "Running",
+    COMPLETED = "Completed",
+    ERROR = "Error",
+    CANCELLED = "Cancelled"
 }
 
 /*****   INTERFACES   *****/
@@ -167,11 +109,69 @@ export interface Pipeline {
     supported_types: string[];
 }
 
-/*****   ENUMS   *****/
-export enum PipelineStatus {
-    PENDING = "Pending",
-    RUNNING = "Running",
-    COMPLETED = "Completed",
-    ERROR = "Error",
-    CANCELLED = "Cancelled"
+/*****   FUNCTIONS   *****/
+export function countArray(items: string[]) {
+    return items.reduce<Counts>((counts, item) => {
+        if (counts[item]) {
+            counts[item] += 1;
+        } else {
+            counts[item] = 1;
+        }
+        return counts;
+    }, Object.create(null));
+}
+
+export function toKeyValue(items: string[]) {
+    return items.reduce<KeyValue>((map, item) => {
+        map[item] = item;
+        return map;
+    }, Object.create(null));
+}
+
+/**
+ * Return a date string in the format "YYYY-MM-DD", if possible.
+ *
+ * @param {string} date Datestring of the form: "Day, DD Mon YYYY HH:MM:SS GMT"
+ */
+export function formatDateString(date: string) {
+    // Pretty general datestring because we trust the server to send a good one
+    const regex = /^[A-Z][a-z]{2}, (\d{2}) ([A-Z][a-z]{2}) (\d{4}) \d{2}:\d{2}:\d{2} GMT$/
+    const result = regex.exec(date);
+    if (result) {
+        let [year, month, day] = [result[3], '' + (months.indexOf(result[2]) + 1), result[1]];
+        if (month.length < 2)
+            month = '0' + month;
+        return [year, month, day].join('-');
+    }
+    return date;
+}
+
+/**
+ * Convert the provided JSON Array to a valid array of Analysis.
+ */
+export function jsonToAnalyses(data: Array<any>): Analysis[] {
+    const rows: Analysis[] = data.map((row, index, arr) => {
+        switch (row.analysis_state) {
+            case 'Requested':
+                row.state = PipelineStatus.PENDING;
+                break;
+            case 'Running':
+                row.state = PipelineStatus.RUNNING;
+                break;
+            case 'Done':
+                row.state = PipelineStatus.COMPLETED;
+                break;
+            case 'Error':
+                row.state = PipelineStatus.ERROR;
+                break;
+            case 'Cancelled':
+                row.state = PipelineStatus.CANCELLED;
+                break;
+            default:
+                row.state = null;
+                break;
+        }
+        return { ...row, selected: false } as Analysis;
+    });
+    return rows;
 }
