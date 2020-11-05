cee91d381fed35711257d5e354d34d230aefa4e1
Anna Guo <annaguo8010@gmail.com>
2020-11-05 14:04:09 -0500

    make sample table more responsive

--- react/src/pages/participants/SampleTable.tsx
+++ react/src/pages/participants/SampleTable.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
 import MaterialTable from "material-table";
-import { formatDateString, Sample } from "../utils";
-import DatasetAccordions from "./DatasetAccordion";
-
+import { formatDateString } from "../utils/functions";
+import { Sample } from "../utils/typings";
+import DatasetList from "./DatasetList";
 const useStyles = makeStyles(theme => ({
-    table: {
-        marginBottom: theme.spacing(3),
+    paper: {
+        // border: "solid red 1px"
     },
 }));
 
@@ -16,39 +16,51 @@ interface SamplesTableProp {
 
 export default function SamplesTable({ samples }: SamplesTableProp) {
     const classes = useStyles();
-
+    const cellStyle = {
+        padding: 0,
+    };
     return (
-        <div className={classes.table}>
-            <MaterialTable
-                columns={[
-                    { title: "Sample ID", field: "tissue_sample_id" },
-                    { title: "Extraction Date", field: "extraction_date" },
-                    { title: "Sample Type", field: "tissue_sample_type" },
-                    { title: "Tissue Processing", field: "tissue_processing" },
-                    { title: "Notes", field: "notes" },
-                    {
-                        title: "Creation Time",
-                        field: "created",
-                        render: rowData => formatDateString(rowData.created),
-                    },
-                    { title: "Create By", field: "created_by" },
-                    {
-                        title: "Update Time",
-                        field: "updated",
-                        render: rowData => formatDateString(rowData.updated),
-                    },
-                    { title: "Updated By", field: "updated_by" },
-                ]}
-                data={samples}
-                title="Samples"
-                detailPanel={rowData => <DatasetAccordions datasets={rowData.datasets} />}
-                options={{
-                    paging: false,
-                    selection: false,
-                    search: false,
-                    padding: "dense",
-                }}
-            />
-        </div>
+        <MaterialTable
+            columns={[
+                { title: "Sample ID", field: "tissue_sample_id", cellStyle: cellStyle },
+                {
+                    title: "Extraction Date",
+                    field: "extraction_date",
+                    cellStyle: cellStyle,
+                    render: rowData => formatDateString(rowData.updated),
+                },
+                { title: "Sample Type", field: "tissue_sample_type", cellStyle: cellStyle },
+                { title: "Tissue Processing", field: "tissue_processing", cellStyle: cellStyle },
+                { title: "Notes", field: "notes", cellStyle: cellStyle },
+                {
+                    title: "Creation Time",
+                    field: "created",
+                    cellStyle: cellStyle,
+                    render: rowData => formatDateString(rowData.created),
+                },
+                { title: "Create By", field: "created_by", cellStyle: cellStyle },
+                {
+                    title: "Update Time",
+                    field: "updated",
+                    cellStyle: cellStyle,
+                    render: rowData => formatDateString(rowData.updated),
+                },
+                { title: "Updated By", field: "updated_by", cellStyle: cellStyle },
+            ]}
+            data={samples}
+            title="Samples"
+            detailPanel={rowData => <DatasetList datasets={rowData.datasets} />}
+            components={{
+                Container: props => <div className={classes.paper}>{props.children}</div>,
+            }}
+            options={{
+                paging: false,
+                selection: false,
+                search: false,
+                headerStyle: {
+                    padding: 0,
+                },
+            }}
+        />
     );
 }
