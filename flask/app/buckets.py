94e559187e4f6d4ee6f0e2b010e1952e7429e9d1
Samarth Patel <samarth patel@CCM-3948-1NML.local>
2020-11-11 11:52:41 -0500

    checked user's groups

--- flask/app/buckets.py
+++ flask/app/buckets.py
@@ -42,30 +42,43 @@ def get_object_info():
 
 
 @app.route("/api/unlinked", methods=["GET"])
+@login_required
 def get_unlinked_files():
 
-    bucket_names = [bucket.name for bucket in minioClient.list_buckets()]
-    # match against buckets/groups the user should be seeing, remove those from the list
-    # note that bucket names are lower case to confrom with s3
+    # Get all minio bucket names
+    all_bucket_names = [bucket.name for bucket in minioClient.list_buckets()]
+
+    # Remove buckets the current user does not have access to assuming group_code.lower() == bucket name
+    user = (
+        db.session.query(models.User)
+        .filter_by(user_id=current_user.user_id)
+        .options(joinedload(models.User.groups))
+        .first()
+    )
+
+    valid_bucket_names = []
+    for group in user.groups:
+        code = group.group_code.lower()
+        if code in all_bucket_names:
+            valid_bucket_names.append(code)
 
     # Get all files in valid minio buckets
     all_files = []
-    for bucket in bucket_names:
+    for bucket in valid_bucket_names:
         objs = minioClient.list_objects(bucket)
         for obj in objs:
-            all_files.append(obj.__dict__['object_name'])
+            all_files.append(obj.__dict__["object_name"])
 
     # Get all linked files
     linked_files = []
-    # there is prob a better way to query just for input_hpc_path actually
     for dataset in models.Dataset.query.all():
         if dataset.input_hpf_path is not None:
             linked_files.append(dataset.input_hpf_path)
 
+    # Put all unlinked files in new list
     unlinked_files = []
     for file_name in all_files:
         if file_name not in linked_files:
             unlinked_files.append(file_name)
-
 
     return jsonify(unlinked_files)
