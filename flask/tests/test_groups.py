ef44c92ab16ec2356285833b2c3576709b1a3b4f
Samarth Patel <samarth28@hotmail.com>
2020-12-07 16:51:02 -0500

    Finished tests for groups endpoints

--- flask/tests/test_groups.py
+++ flask/tests/test_groups.py
@@ -2,7 +2,40 @@ import pytest
 from app import db, models
 from sqlalchemy.orm import joinedload
 from conftest import TestConfig
-from app.madmin import MinioAdmin
+from app.madmin import MinioAdmin, readwrite_buckets_policy
+from minio import Minio
+
+@pytest.fixture
+def minioAdmin():
+    madmin = MinioAdmin(
+        endpoint=TestConfig.MINIO_ENDPOINT,
+        access_key=TestConfig.MINIO_ACCESS_KEY,
+        secret_key=TestConfig.MINIO_SECRET_KEY,
+    )
+    # To match test_database on the minio side
+    madmin.add_user("user", "useruser")
+    madmin.add_user("admin", "adminadmin")
+    madmin.group_add("ach", "user")
+    madmin.add_policy("ach", readwrite_buckets_policy("ach"))
+    madmin.set_policy("ach", group="ach")
+    yield madmin
+    # Teardown
+    try:
+        madmin.remove_user("user")
+    except:
+        pass
+    try:
+        madmin.remove_user("admin")
+    except:
+        pass
+    try:
+        madmin.group_remove("ach")
+    except:
+        pass
+    try:
+        madmin.remove_policy("ach")
+    except:
+        pass
 
 # Tests
 
@@ -70,15 +103,7 @@ def test_get_group(test_database, client, login_as):
 # DELETE /api/groups/:id
 
 
-def test_delete_group(test_database, client, login_as):
-    # Simulate minio group equivalent of test_database
-    minioAdmin = MinioAdmin(
-        endpoint=TestConfig.MINIO_ENDPOINT,
-        access_key=TestConfig.MINIO_ACCESS_KEY,
-        secret_key=TestConfig.MINIO_SECRET_KEY,
-    )
-    minioAdmin.add_user("user", "useruser")
-    minioAdmin.group_add("ach", "user")
+def test_delete_group(test_database, client, login_as, minioAdmin):
 
     # Test without permission
     login_as("user")
@@ -111,84 +136,124 @@ def test_delete_group(test_database, client, login_as):
     assert len(minioAdmin.list_groups()) == 0
 
 
-"""
+
 
 
 # PATCH /api/groups/:id
 
 
-def test_update_family(test_database, client, login_as):
-    login_as("user")
+def test_update_group(test_database, client, login_as, minioAdmin):
+    login_as("admin")
     # Test existence
     assert (
-        client.patch("/api/groups/4", json={"family_codename": "C"}).status_code
+        client.patch("/api/groups/code", json={"group_name": "yes"}).status_code
         == 404
     )
 
-    # Test permission
+    # Test success and check db and minio
+    ad = models.User.query.filter(models.User.username == "admin").one_or_none()
+    ad.minio_access_key = "admin"
+    user = models.User.query.filter(models.User.username == "user").one_or_none()
+    user.minio_access_key = "user"
     assert (
-        client.patch("/api/groups/2", json={"family_codename": "C"}).status_code
-        == 404
-    )
-
-    # Test no codename error
-    assert (
-        client.patch(
-            "/api/groups/2",
-            json={"Haha you thought it was a codename": "but it was I, useless json"},
-        ).status_code
-        == 400
-    )
-
-    # Test success
-    assert (
-        client.patch("/api/groups/1", json={"family_codename": "C"}).status_code
+        client.patch("/api/groups/ach", json={"group_name": "Alberta2", "users": ["admin"]}).status_code
         == 200
     )
     # Make sure it updated
-    family = models.Family.query.filter(models.Family.family_id == 1).one_or_none()
-    assert family.family_codename == "C"
+    group = models.Group.query.filter(models.Group.group_name == "Alberta2").one_or_none()
+    assert group is not None
+    assert len(group.users) == 1
+    assert group.users[0].username == "admin"
+
+    # Reset
+    assert (
+        client.patch("/api/groups/ach", json={"group_name": "Alberta", "users": ["user"]}).status_code
+        == 200
+    )
 
 
 # POST /api/groups
 
 
-def test_create_family(test_database, client, login_as):
+def test_create_group(test_database, client, login_as, minioAdmin):
     login_as("user")
     assert client.post("/api/groups").status_code == 401
 
     login_as("admin")
-    # Test no codename given
+    # Test no group_code given
     assert (
         client.post(
             "/api/groups",
-            json={"Haha you thought it was a codename": "but it was I, useless json"},
+            json={"A json you say?": "How could you possibly be so naive"},
         ).status_code
         == 400
     )
-    # Test codename already in use
+    # Test group_code already in use
     assert (
         client.post(
             "/api/groups",
-            json={"family_codename": "A"},
+            json={"group_code": "ach", "group_name": "Nothing suspicious"},
         ).status_code
         == 422
     )
-    # Test success and check db
+
+    # Test success with no users and check db and minio
     assert (
         client.post(
             "/api/groups",
             json={
-                "family_codename": "C",
+                "group_code": "code",
+                "group_name": "Actually nothing suspicious"
             },
         ).status_code
         == 201
     )
-    family = models.Family.query.filter(
-        models.Family.family_codename == "C"
+    group = models.Group.query.filter(
+        models.Group.group_code == "code"
     ).one_or_none()
-    assert family is not None
+    assert group is not None
+    assert len(minioAdmin.list_policies()) == 6
+    minioClient = Minio(
+        TestConfig.MINIO_ENDPOINT,
+        access_key=TestConfig.MINIO_ACCESS_KEY,
+        secret_key=TestConfig.MINIO_SECRET_KEY,
+        secure=False,
+    )
+    # Clean up
+    minioClient.remove_bucket("code")
+    minioAdmin.remove_policy("code")
 
-    login_as("admin")
-    assert len(client.get("/api/groups").get_json()) == 3
-"""
+    # Test success with users and check db and minio
+    user = models.User.query.filter(models.User.username == "user").one_or_none()
+    user.minio_access_key = "user"
+
+    assert (
+        client.post(
+            "/api/groups",
+            json={
+                "group_code": "code2",
+                "group_name": "Actually nothing else suspicious",
+                "users": [
+                    "user"
+                ]
+            },
+        ).status_code
+        == 201
+    )
+    group = models.Group.query.filter(
+        models.Group.group_code == "code2"
+    ).one_or_none()
+    assert group is not None
+    assert len(minioAdmin.list_policies()) == 6
+    assert len(minioAdmin.get_group("code2")["members"]) == 1
+    minioClient = Minio(
+        TestConfig.MINIO_ENDPOINT,
+        access_key=TestConfig.MINIO_ACCESS_KEY,
+        secret_key=TestConfig.MINIO_SECRET_KEY,
+        secure=False,
+    )
+    # Clean up
+    minioClient.remove_bucket("code2")
+    minioAdmin.remove_policy("code2")
+    minioAdmin.remove_user("user")
+    minioAdmin.group_remove("code2")
