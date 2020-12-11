e9b32ecb51a167608cd0a5fca1a9f3dc921ce97d
Mackenzie Frew <11793063+frewmack@users.noreply.github.com>
2020-12-11 14:00:01 -0500

    Add prop for setting column width in dialog info

--- react/src/pages/utils/components/GridFieldsDisplay.tsx
+++ react/src/pages/utils/components/GridFieldsDisplay.tsx
@@ -3,16 +3,21 @@ import { Grid } from "@material-ui/core";
 import { Field } from "../typings";
 import FieldDisplayEditable from "./FieldDisplayEditable";
 
-const infoWidth = 6;
+// Yes, really
+type Width = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 /**
- * Display a collection of Fields in two columns in the specified order.
+ * Display a collection of editable fields. Number of columns can be specified.
+ *
+ * @param orderPriority How to display the fields (left-right first or top-down first)
+ * @param columnWidth The width of each column (out of 12, must be an integer)
  */
 export default function GridFieldsDisplay(props: {
     fields: Field[];
     editMode: boolean;
     onEdit: (fieldName: string | undefined, value: any) => void;
     orderPriority?: "left-right" | "top-down";
+    columnWidth?: Width;
 }) {
     const [enums, setEnums] = useState(undefined);
 
@@ -29,46 +34,65 @@ export default function GridFieldsDisplay(props: {
         });
     }, []);
 
-    let leftFields: Field[] = [];
-    let rightFields: Field[] = [];
-    const cap = Math.ceil(props.fields.length / 2);
+    const infoWidth = props.columnWidth || 6;
+    const numColumns = Math.floor(12 / infoWidth);
+
+    let fieldColumns: Field[][] = [...Array(numColumns)].map(() => []);
 
     if (props.orderPriority === "left-right") {
-        for (let i = 0; i < props.fields.length; i += 2) {
-            const j = i + 1;
-            leftFields.push(props.fields[i]);
-            if (j < props.fields.length) rightFields.push(props.fields[j]);
+        for (let i = 0; i < props.fields.length; i += numColumns) {
+            for (let j = 0; j < numColumns; j++) {
+                if (i + j < props.fields.length) fieldColumns[j].push(props.fields[i + j]);
+            }
         }
     } else {
-        for (let i = 0; i < cap; i++) {
-            const j = cap + i;
-            leftFields.push(props.fields[i]);
-            if (j < props.fields.length) rightFields.push(props.fields[j]);
+        const cap = Math.ceil(props.fields.length / numColumns);
+        for (let i = 0; i < numColumns; i++) {
+            for (let j = 0; j < cap; j++) {
+                if (i + j < props.fields.length) fieldColumns[i].push(props.fields[i + j]);
+            }
         }
     }
 
     return (
         <>
-            <Grid item xs={infoWidth}>
-                {leftFields.map(field => (
-                    <FieldDisplayEditable
-                        field={field}
-                        editMode={props.editMode}
-                        enums={enums}
-                        onEdit={props.onEdit}
-                    />
-                ))}
-            </Grid>
-            <Grid item xs={infoWidth}>
-                {rightFields.map(field => (
-                    <FieldDisplayEditable
-                        field={field}
-                        editMode={props.editMode}
-                        enums={enums}
-                        onEdit={props.onEdit}
-                    />
-                ))}
-            </Grid>
+            {fieldColumns.map(column => (
+                <Grid item xs={infoWidth}>
+                    {column.map(field => (
+                        <FieldDisplayEditable
+                            field={field}
+                            editMode={props.editMode}
+                            enums={enums}
+                            onEdit={props.onEdit}
+                        />
+                    ))}
+                </Grid>
+            ))}
         </>
     );
+
+    // return (
+    //     <>
+    //         <Grid item xs={infoWidth}>
+    //             {leftFields.map(field => (
+    //                 <FieldDisplayEditable
+    //                     field={field}
+    //                     editMode={props.editMode}
+    //                     enums={enums}
+    //                     onEdit={props.onEdit}
+    //                 />
+    //             ))}
+    //         </Grid>
+    //         <Grid item xs={infoWidth}>
+    //             {rightFields.map(field => (
+    //                 <FieldDisplayEditable
+    //                     field={field}
+    //                     editMode={props.editMode}
+    //                     enums={enums}
+    //                     onEdit={props.onEdit}
+    //                 />
+    //             ))}
+    //         </Grid>
+    //     </>
+    // );
 }
