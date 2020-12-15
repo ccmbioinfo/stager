50b31af74e1f8e66e133808324f7064d0e76ceb9
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2020-12-15 15:08:15 -0500

    Flask: add GET /api/users/:username

--- flask/app/users.py
+++ flask/app/users.py
@@ -1,5 +1,5 @@
 from flask import current_app as app, jsonify
-from flask_login import login_required
+from flask_login import current_user, login_required
 from sqlalchemy.orm import joinedload
 
 from . import models
@@ -23,4 +23,34 @@ def list_users():
             }
             for user in users
         ]
+    )
+
+
+@app.route("/api/users/<string:username>", methods=["GET"])
+@login_required
+def get_user(username: str):
+    if (
+        not app.config.get("LOGIN_DISABLED")
+        and not current_user.is_admin
+        and username != current_user.username
+    ):
+        return "Unauthorized", 401
+
+    user = (
+        models.User.query.options(joinedload(models.User.groups))
+        .filter(models.User.username == username)
+        .first_or_404()
+    )
+
+    return jsonify(
+        {
+            "username": user.username,
+            "email": user.email,
+            "is_admin": user.is_admin,
+            "last_login": user.last_login,
+            "deactivated": user.deactivated,
+            "groups": [group.group_code for group in user.groups],
+            "minio_access_key": user.minio_access_key,
+            "minio_secret_key": user.minio_secret_key,
+        }
     )
--- flask/tests/test_users.py
+++ flask/tests/test_users.py
@@ -1,6 +1,24 @@
 import pytest
 
 
+# Common response values between list and individual get endpoints
+expected_admin_common = {
+    "username": "admin",
+    "email": "noreply@sickkids.ca",
+    "is_admin": True,
+    "deactivated": False,
+    "groups": [],
+}
+
+expected_user_common = {
+    "username": "user",
+    "email": "test@sickkids.ca",
+    "is_admin": False,
+    "deactivated": False,
+    "groups": ["ach"],
+}
+
+
 def test_list_users(test_database, client, login_as):
     login_as("admin")
     response = client.get("/api/users")
@@ -14,20 +32,12 @@ def test_list_users(test_database, client, login_as):
 
     assert user_list == [
         {
-            "username": "admin",
-            "email": "noreply@sickkids.ca",
-            "is_admin": True,
+            **expected_admin_common,
             "last_login": user_list[0]["last_login"],  # don't know this value
-            "deactivated": False,
-            "groups": [],
         },
         {
-            "username": "user",
-            "email": "test@sickkids.ca",
-            "is_admin": False,
+            **expected_user_common,
             "last_login": user_list[1]["last_login"],
-            "deactivated": False,
-            "groups": ["ach"],
         },
     ]
 
@@ -35,3 +45,47 @@ def test_list_users(test_database, client, login_as):
 def test_list_users_unauthorized(test_database, client, login_as):
     login_as("user")
     assert client.get("/api/users").status_code == 401
+
+
+def test_get_user_admin(test_database, client, login_as):
+    login_as("admin")
+    assert client.get("/api/users/foo").status_code == 404
+
+    response = client.get("/api/users/admin")
+    assert response.status_code == 200
+    admin = response.get_json()
+    assert admin == {
+        **expected_admin_common,
+        "last_login": admin["last_login"],  # don't know this value
+        "minio_access_key": "admin",
+        "minio_secret_key": None,  # usually either both are set or both are null
+    }
+
+    response = client.get("/api/users/user")
+    assert response.status_code == 200
+    user = response.get_json()
+    assert user == {
+        **expected_user_common,
+        "last_login": user["last_login"],  # don't know this value
+        "minio_access_key": "user",
+        "minio_secret_key": None,  # usually either both are set or both are null
+    }
+
+
+def test_get_user_user(test_database, client, login_as):
+    assert client.get("/api/users/foo").status_code == 401
+    assert client.get("/api/users/user").status_code == 401
+
+    login_as("user")
+    assert client.get("/api/users/foo").status_code == 401
+    assert client.get("/api/users/admin").status_code == 401
+
+    response = client.get("/api/users/user")
+    assert response.status_code == 200
+    user = response.get_json()
+    assert user == {
+        **expected_user_common,
+        "last_login": user["last_login"],  # don't know this value
+        "minio_access_key": "user",
+        "minio_secret_key": None,  # usually either both are set or both are null
+    }
