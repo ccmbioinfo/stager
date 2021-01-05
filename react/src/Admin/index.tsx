e194348b436d628004aef5431fc0d1adccf7af16
Anna Guo <annaguo8010@gmail.com>
2021-01-05 12:44:48 -0500

    remove uploaders tab in admin page

--- react/src/Admin/index.tsx
+++ react/src/Admin/index.tsx
@@ -1,40 +1,6 @@
 import React from "react";
-import { makeStyles, Theme, AppBar, Tabs, Tab, Typography, Box } from "@material-ui/core";
+import { makeStyles, Theme, Container, Box } from "@material-ui/core";
 import UserList from "./components/UserList";
-import ManagedUploaders from "./components/ManagedUploaders";
-
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
-}
-
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                <Box p={3}>
-                    <Typography>{children}</Typography>
-                </Box>
-            )}
-        </div>
-    );
-}
-
-function a11yProps(index: any) {
-    return {
-        id: `simple-tab-${index}`,
-        "aria-controls": `simple-tabpanel-${index}`,
-    };
-}
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
@@ -42,31 +8,21 @@ const useStyles = makeStyles((theme: Theme) => ({
         overflow: "auto",
     },
     appBarSpacer: theme.mixins.toolbar,
+    container: {
+        marginTop: theme.spacing(3),
+        marginBottom: theme.spacing(3),
+    },
 }));
 
 export default function Admin() {
     const classes = useStyles();
-    const [currentTab, setCurrentTab] = React.useState(0);
 
     return (
         <Box className={classes.root}>
             <div className={classes.appBarSpacer} />
-            <AppBar position="static">
-                <Tabs
-                    value={currentTab}
-                    onChange={(e, tab) => setCurrentTab(tab)}
-                    aria-label="admin-tabs"
-                >
-                    <Tab label="Manage Users" {...a11yProps(0)} />
-                    <Tab label="Manage Uploaders" {...a11yProps(1)} />
-                </Tabs>
-            </AppBar>
-            <TabPanel value={currentTab} index={0}>
+            <Container className={classes.container} maxWidth={false}>
                 <UserList />
-            </TabPanel>
-            <TabPanel value={currentTab} index={1}>
-                <ManagedUploaders />
-            </TabPanel>
+            </Container>
         </Box>
     );
 }
