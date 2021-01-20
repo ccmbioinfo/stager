0ae3844897b9d32d2135b251305c5f583e18c518
Delvin So <so.delvin@gmail.com>
2021-01-20 11:19:59 -0500

    Flask: restrict user actions for analyses (#290)
    * restrict creating an analysis to datasets a user has access to
    
    * add and fix tests
    
    * restrict analysis state changes to admin, and add tests
    
    * Rewrite POST /api/analyses to match the other create endpoints
    
    * Match output format to GET /api/analyses/:id
    
    note #220
    
    Co-authored-by: Kevin Lu <6320810+kevinlul@users.noreply.github.com>

--- flask/app/analyses.py
+++ flask/app/analyses.py
@@ -152,68 +152,88 @@ def get_analysis(id: int):
 def create_analysis():
     if not request.json:
         return "Request body must be JSON!", 415
-    try:
-        dts_pks = request.json["datasets"]
-    except KeyError:
-        return "No Dataset field provided", 400
-    try:
-        pipeline_pk = request.json["pipeline_id"]
-    except KeyError:
-        return "No Pipeline field provided", 400
 
-    if not dts_pks:
-        return "No Dataset IDs provided", 400
+    pipeline_id = request.json.get("pipeline_id")
+    if not isinstance(pipeline_id, int):
+        return "Missing pipeline_id field or invalid type", 400
 
-    if not pipeline_pk:
-        return "No Pipeline ID provided", 400
+    datasets = request.json.get("datasets")
+    if not (isinstance(datasets, list) and len(datasets)):
+        return "Missing datasets field or invalid type", 400
 
-    pipeline_id = models.Pipeline.query.get(pipeline_pk).pipeline_id
+    if not models.Pipeline.query.get(pipeline_id):
+        return "Pipeline not found", 404
+
+    if app.config.get("LOGIN_DISABLED"):
+        user_id = request.args.get("user")
+        requester_id = updated_by_id = user_id or 1
+    elif current_user.is_admin:
+        user_id = request.args.get("user")
+        requester_id = updated_by_id = user_id or current_user.user_id
+    else:
+        requester_id = updated_by_id = user_id = current_user.user_id
+
+    if user_id:
+        found_datasets_query = (
+            models.Dataset.query.join(
+                models.groups_datasets_table,
+                models.Dataset.dataset_id
+                == models.groups_datasets_table.columns.dataset_id,
+            )
+            .join(
+                models.users_groups_table,
+                models.groups_datasets_table.columns.group_id
+                == models.users_groups_table.columns.group_id,
+            )
+            .filter(models.users_groups_table.columns.user_id == user_id)
+        )
+    else:
+        found_datasets_query = models.Dataset.query
+
+    found_datasets = found_datasets_query.filter(
+        models.Dataset.dataset_id.in_(datasets)
+    ).all()
+    if len(found_datasets) != len(datasets):
+        return "Some datasets were not found", 404
 
     now = datetime.now()
-    requested = now
-    updated = now
-    analysis_state = "Requested"
-    try:
-        requester_id = updated_by_id = current_user.user_id
-    except:  # LOGIN DISABLED
-        requester_id = updated_by_id = 1
-
-    obj = models.Analysis(
-        **{
-            "requested": requested,
-            "analysis_state": analysis_state,
-            "requester_id": requester_id,
-            "updated_by_id": updated_by_id,
-            "updated": updated,
-            "requested": requested,
-            "pipeline_id": pipeline_id,
-        }
+    analysis = models.Analysis(
+        analysis_state="Requested",
+        pipeline_id=pipeline_id,
+        requester_id=requester_id,
+        requested=now,
+        updated=now,
+        updated_by_id=updated_by_id,
+        datasets=found_datasets,
     )
-
-    db.session.add(obj)
-    transaction_or_abort(db.session.flush)
-
-    # update the dataset_analyses table
-    dataset_analyses_obj = [
-        {"dataset_id": x, "analysis_id": obj.analysis_id} for x in dts_pks
-    ]
-
-    inst = models.datasets_analyses_table.insert().values(dataset_analyses_obj)
-    try:
-        db.session.execute(inst)
-    except:
-        db.session.rollback()
-        return "Server error", 500
-
+    db.session.add(analysis)
     transaction_or_abort(db.session.commit)
 
-    return jsonify(
-        {
-            **asdict(obj),
-            "assignee": obj.assignee_id and obj.assignee.username,
-            "requester": obj.requester_id and obj.requester.username,
-            "updated_by": obj.updated_by_id and obj.updated_by.username,
-        }
+    return (
+        jsonify(
+            {
+                **asdict(analysis),
+                "requester": analysis.requester_id and analysis.requester.username,
+                "updated_by": analysis.updated_by_id and analysis.updated_by.username,
+                "assignee": analysis.assignee_id and analysis.assignee.username,
+                "pipeline": analysis.pipeline,
+                "datasets": [
+                    {
+                        **asdict(dataset),
+                        "tissue_sample_type": dataset.tissue_sample.tissue_sample_type,
+                        "participant_codename": dataset.tissue_sample.participant.participant_codename,
+                        "participant_type": dataset.tissue_sample.participant.participant_type,
+                        "sex": dataset.tissue_sample.participant.sex,
+                        "family_codename": dataset.tissue_sample.participant.family.family_codename,
+                        "updated_by": dataset.tissue_sample.updated_by.username,
+                        "created_by": dataset.tissue_sample.created_by.username,
+                    }
+                    for dataset in analysis.datasets
+                ],
+            }
+        ),
+        201,
+        {"location": f"/api/analyses/{analysis.analysis_id}"},
     )
 
 
@@ -244,6 +264,11 @@ def update_analysis(id: int):
         user_id = request.args.get("user")
     else:
         user_id = current_user.user_id
+        if request.json.get("analysis_state") not in [
+            "Cancelled",
+            None,  # account for default
+        ]:
+            return "Analysis state changes are restricted to administrators", 403
 
     if user_id:
         analysis = (
