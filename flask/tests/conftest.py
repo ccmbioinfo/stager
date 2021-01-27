29be844f1216733524f5f4957dec262886aea6a2
Delvin So <so.delvin@gmail.com>
2021-01-27 16:06:11 -0500

    update tests

--- flask/tests/conftest.py
+++ flask/tests/conftest.py
@@ -167,6 +167,20 @@ def test_database(client):
 
     pipeline_1 = Pipeline(pipeline_name="CRG", pipeline_version="1.2")
     db.session.add(pipeline_1)
+    pipeline_2 = Pipeline(pipeline_name="CRE", pipeline_version="1.1")
+    db.session.add(pipeline_2)
+    db.session.flush()
+
+    db.session.add(
+        PipelineDatasets(
+            pipeline_id=pipeline_1.pipeline_id, supported_metadataset_type="Genome"
+        )
+    )
+    db.session.add(
+        PipelineDatasets(
+            pipeline_id=pipeline_2.pipeline_id, supported_metadataset_type="Exome"
+        )
+    )
     db.session.flush()
 
     family_a = Family(
