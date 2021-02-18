48d179c2e29cf29cc775b755f43acea2a805c787
Mackenzie <11793063+frewmack@users.noreply.github.com>
2021-02-18 08:44:45 -0500

    Update ListItemRouterLink to include tooltips

--- react/src/components/ListItemRouterLink.tsx
+++ react/src/components/ListItemRouterLink.tsx
@@ -1,27 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
+import { ListItem, ListItemIcon, ListItemText, Tooltip } from "@material-ui/core";
 
 export interface ListItemRouterLinkProps {
-    primary: React.ReactNode;
+    primary: string;
     to: string;
     children: React.ReactNode;
+    hideTooltip: boolean;
 }
 
 // https://material-ui.com/guides/composition/#caveat-with-inlining
-export default function ListItemRouterLink({ primary, to, children }: ListItemRouterLinkProps) {
-    const ForwardedRouterLink = React.useMemo(
-        () =>
-            React.forwardRef<HTMLAnchorElement>((linkProps, ref) => (
-                <Link ref={ref} to={to} {...linkProps} />
-            )),
-        [to]
-    );
+// https://material-ui.com/components/tooltips/#custom-child-element
+const ListItemRouterLink = React.forwardRef<HTMLDivElement, ListItemRouterLinkProps>(
+    function _ListItemRouterLink(props, ref) {
+        const [openTooltip, setOpenTooltip] = useState(false);
 
-    return (
-        <ListItem button component={ForwardedRouterLink}>
-            <ListItemIcon>{children}</ListItemIcon>
-            <ListItemText primary={primary} />
-        </ListItem>
-    );
-}
+        const ForwardedRouterLink = React.useMemo(
+            () =>
+                React.forwardRef<HTMLAnchorElement>((linkProps, linkRef) => (
+                    <Link ref={linkRef} to={props.to} {...linkProps} />
+                )),
+            [props.to]
+        );
+
+        return (
+            <Tooltip arrow title={props.primary} placement="right" open={openTooltip}>
+                <ListItem
+                    button
+                    component={ForwardedRouterLink}
+                    ContainerProps={{
+                        onMouseOver: () => setOpenTooltip(true && props.hideTooltip),
+                        onMouseLeave: () => setOpenTooltip(false),
+                    }}
+                >
+                    <ListItemIcon ref={ref}>{props.children}</ListItemIcon>
+                    <ListItemText primary={props.primary} />
+                </ListItem>
+            </Tooltip>
+        );
+    }
+);
+
+export default ListItemRouterLink;
