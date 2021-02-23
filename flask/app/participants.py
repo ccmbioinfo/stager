f5eb494cc8b51b28fb923ea6416572da5e725275
Madeline Couse <mhcouse@gmail.com>
2021-02-23 11:28:38 -0500

    logging on all endpoints

--- flask/app/participants.py
+++ flask/app/participants.py
@@ -39,14 +39,19 @@ participants_blueprint = Blueprint(
 @participants_blueprint.route("/api/participants", methods=["GET"])
 @login_required
 def list_participants() -> Response:
+
+    app.logger.debug("Parsing paged query parameters")
     # General paged query parameters
     # for some reason type=int doesn't catch non-integer queries, only returning None
+    app.logger.debug("Validating page")
     try:
         page = int(request.args.get("page", default=0))
         if page < 0:  # zero-indexed pages
             raise ValueError
     except:
+        app.logger.error("page must be a non-negative integer")
         abort(400, description="page must be a non-negative integer")
+    app.logger.debug("Validating limit...")
     try:
         limit = request.args.get("limit")
         if limit is not None:  # unspecified limit means return everything
@@ -54,8 +59,11 @@ def list_participants() -> Response:
             if limit <= 0:  # MySQL accepts 0 but that's just a waste of time
                 raise ValueError
     except:
+        app.logger.error("limit must be a positive integer")
         abort(400, description="limit must be a positive integer")
 
+    app.logger.debug("Parsing order query parameters")
+    app.logger.debug("Validating order_by")
     order_by = request.args.get("order_by", type=str)
     allowed_columns = [
         "family_codename",
@@ -75,8 +83,10 @@ def list_participants() -> Response:
         # order_by has been restricted to a known list, so we can safely use getattr
         order = getattr(models.Participant, order_by)
     else:
+        app.logger.error("order_by must be one of %s", allowed_columns)
         abort(400, description=f"order_by must be one of {allowed_columns}")
 
+    app.logger.debug("Validating order_dir")
     if order:
         order_dir = request.args.get("order_dir", type=str)
         if order_dir == "desc":
@@ -84,21 +94,27 @@ def list_participants() -> Response:
         elif order_dir == "asc":
             order = order.asc()
         else:
+            app.logger.error("order_dir must be one either 'asc', 'desc'")
             abort(400, description="order_dir must be either 'asc' or 'desc'")
 
+    app.logger.debug("Parsing filter query parameters")
     filters = []
     family_codename = request.args.get("family_codename", type=str)
+    app.logger.debug("Parsing family_codename: %s", family_codename)
     if family_codename:
         filters.append(func.instr(models.Family.family_codename, family_codename))
     participant_codename = request.args.get("participant_codename", type=str)
+    app.logger.debug("Parsing participant_codename: %s", participant_codename)
     if participant_codename:
         filters.append(
             func.instr(models.Participant.participant_codename, participant_codename)
         )
     notes = request.args.get("notes", type=str)
+    app.logger.debug("Parsing notes: %s", notes)
     if notes:
         filters.append(func.instr(models.Participant.notes, notes))
     participant_type = request.args.get("participant_type", type=str)
+    app.logger.debug("Parsing participant_type: %s", participant_type)
     if participant_type:
         filters.append(
             filter_in_enum_or_abort(
@@ -108,22 +124,30 @@ def list_participants() -> Response:
             )
         )
     sex = request.args.get("sex", type=str)
+    app.logger.debug("Parsing sex: %s", sex)
     if sex:
         filters.append(filter_in_enum_or_abort(models.Participant.sex, models.Sex, sex))
     affected = request.args.get("affected", type=str)
+    app.logger.debug("Parsing affected: %s", affected)
     if affected:
         filters.append(
             filter_nullable_bool_or_abort(models.Participant.affected, affected)
         )
     solved = request.args.get("solved", type=str)
+    app.logger.debug("Parsing solved: %s", solved)
     if solved:
         filters.append(filter_nullable_bool_or_abort(models.Participant.solved, solved))
 
+    app.logger.debug("Retrieving user id")
     if app.config.get("LOGIN_DISABLED") or current_user.is_admin:
         user_id = request.args.get("user")
+        app.logger.debug("User is admin with ID '%s'", user_id)
     else:
         user_id = current_user.user_id
+        app.logger.debug("User is regular with ID '%s'", user_id)
+
     if user_id:  # Regular user or assumed identity, return only permitted participants
+        app.logger.debug("Processing query - restricted based on user id %s.", user_id)
         query = (
             models.Participant.query.options(
                 joinedload(models.Participant.institution),
@@ -148,6 +172,7 @@ def list_participants() -> Response:
             .filter(models.users_groups_table.columns.user_id == user_id, *filters)
         )
     else:  # Admin or LOGIN_DISABLED, authorized to query all participants
+        app.logger.debug("Processing query - unrestricted based on user id")
         query = (
             models.Participant.query.options(
                 joinedload(models.Participant.institution),
@@ -165,11 +190,13 @@ def list_participants() -> Response:
     # mapped objects. In addition, .count() just wraps the main query in a subquery, so it can be
     # inefficient. Luckily, we can sidestep this whole problem efficiently by having the database
     # count the number of distinct parent primary keys returned. https://gist.github.com/hest/8798884
+    app.logger.debug("Counting number of participants")
     total_count = query.with_entities(
         func.count(distinct(models.Participant.participant_id))
     ).scalar()
     participants = query.order_by(order).limit(limit).offset(page * (limit or 0)).all()
 
+    app.logger.debug("Query successful, returning JSON")
     return jsonify(
         {
             "data": [
@@ -198,6 +225,7 @@ def list_participants() -> Response:
 @login_required
 @check_admin
 def delete_participant(id: int):
+    app.logger.debug("DELETE request for participant id '%s'", id)
     participant = (
         models.Participant.query.filter(models.Participant.participant_id == id)
         .options(joinedload(models.Participant.tissue_samples))
@@ -207,11 +235,14 @@ def delete_participant(id: int):
         try:
             db.session.delete(participant)
             db.session.commit()
+            app.logger.debug("DELETE request for participant id '%s' succeeded", id)
             return "Updated", 204
         except:
             db.session.rollback()
+            app.logger.error("Server error")
             abort(500, description="Server error")
     else:
+        app.logger.error("Query failed as participant has associated tissue samples")
         abort(422, description="Participant has tissue samples, cannot delete")
 
 
@@ -219,15 +250,22 @@ def delete_participant(id: int):
 @login_required
 def update_participant(id: int):
 
+    app.logger.debug("Checking request body")
     if not request.json:
+        app.logger.error("Request body is not JSON")
         abort(415, description="Request body must be JSON")
+    app.logger.debug("Request body is JSON")
 
+    app.logger.debug("Retrieving user id")
     if app.config.get("LOGIN_DISABLED") or current_user.is_admin:
         user_id = request.args.get("user")
+        app.logger.debug("User is admin with ID '%s'", user_id)
     else:
         user_id = current_user.user_id
+        app.logger.debug("User is regular with ID '%s'", user_id)
 
     if user_id:
+        app.logger.debug("Processing query - restricted based on user id")
         participant = (
             models.Participant.query.filter(models.Participant.participant_id == id)
             .join(models.TissueSample)
@@ -246,6 +284,7 @@ def update_participant(id: int):
             .first_or_404()
         )
     else:
+        app.logger.debug("Processing query - unrestricted based on user id")
         participant = models.Participant.query.filter(
             models.Participant.participant_id == id
         ).first_or_404()
@@ -253,13 +292,16 @@ def update_participant(id: int):
     enum_error = mixin(participant, request.json, editable_columns)
 
     if enum_error:
+        app.logger.error("%s", enum_error)
         abort(400, description="enum_error")
 
+    app.logger.debug("Participant update is peformed by user: '%s'", user_id)
     if user_id:
         participant.updated_by_id = user_id
 
     transaction_or_abort(db.session.commit)
 
+    app.logger.debug("Update successful, returning JSON")
     return jsonify(
         [
             {
@@ -278,18 +320,26 @@ def update_participant(id: int):
 @login_required
 @check_admin
 def create_participant():
+    app.logger.debug("Checking request body")
     if not request.json:
+        app.logger.error("Request body is not JSON")
         abort(415, description="Request body must be JSON")
-
+    app.logger.debug("Request body is JSON")
     try:
+        app.logger.debug(
+            "Assigning updated and created by IDs to user ID '%s'", current_user.user_id
+        )
         updated_by_id = current_user.user_id
         created_by_id = current_user.user_id
     except:  # LOGIN_DISABLED
+        app.logger.debug(
+            "LOGIN_DISABLED = True so updated and created by IDs are defaulting to 1"
+        )
         updated_by_id = 1
         created_by_id = 1
 
     # check if the participant exists under a given family
-
+    app.logger.debug("Checking if participant already exists under family")
     ptp_query = models.Participant.query.filter(
         models.Participant.family_id == request.json.get("family_id"),
         models.Participant.participant_codename
@@ -297,9 +347,11 @@ def create_participant():
     )
 
     if ptp_query.first() is not None:
+        app.logger.error("Participant codename already exists under family")
         abort(422, description="Participant codename already exists under family")
 
     # check if family exists
+    app.logger.debug("Checking if family exists")
     models.Family.query.filter(
         models.Family.family_id == request.json.get("family_id")
     ).first_or_404()
@@ -308,22 +360,27 @@ def create_participant():
     enum_error = enum_validate(models.Participant, request.json, editable_columns)
 
     if enum_error:
+        app.logger.error("%s", enum_error)
         abort(400, description=enum_error)
 
     # get institution id
+    app.logger.debug("Check if institution exists")
     institution = request.json.get("institution")
     if institution:
         institution_obj = models.Institution.query.filter(
             models.Institution.institution == institution
         ).one_or_none()
         if institution_obj:
+            app.logger.debug("Institution exists, getting id")
             institution_id = institution_obj.institution_id
         else:
+            app.logger.debug("Create new institution")
             institution_obj = models.Institution(institution=institution)
             db.session.add(institution_obj)
             transaction_or_abort(db.session.flush)
             institution_id = institution_obj.institution_id
 
+    app.logger.debug("Creating an instance of the participant model")
     ptp_objs = models.Participant(
         family_id=request.json.get("family_id"),
         participant_codename=request.json.get("participant_codename"),
@@ -338,11 +395,13 @@ def create_participant():
         updated_by_id=updated_by_id,
     )
 
+    app.logger.debug("Adding the participant instance to the database")
     db.session.add(ptp_objs)
     transaction_or_abort(db.session.commit)
 
     location_header = "/api/participants/{}".format(ptp_objs.participant_id)
 
+    app.logger.debug("Participant successfully added to the database. Returning JSON")
     return (
         jsonify(
             {
