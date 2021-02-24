89d16bdfe788f0b9a2b7551b5000e90c3ed672d1
Delvin So <so.delvin@gmail.com>
2021-02-24 09:57:56 -0500

    Flask: add logging to rendpoints in routes

--- flask/app/routes.py
+++ flask/app/routes.py
@@ -25,15 +25,18 @@ def load_user(uid: int):
 @routes.route("/api/login", methods=["POST"])
 def login():
     last_login = None
+    app.logger.info("Checking whether user is authenticated")
     if current_user.is_authenticated:
         # get/update last login
+        app.logger.info("User is authenticated, updating last login..")
         try:
             last_login = current_user.last_login
             current_user.last_login = datetime.now()
             db.session.commit()
+            app.logger.info("Last login for '%s' updated..", current_user.username)
         except:
             app.logger.warning(
-                "Failed to updated last_login for %s", current_user.username
+                "Failed to updated last_login for '%s'", current_user.username
             )
 
         return jsonify(
@@ -44,13 +47,16 @@ def login():
                 "groups": [group.group_code for group in current_user.groups],
             }
         )
-
+    app.logger.debug("Checking request body")
     body = request.json
     if not body or "username" not in body or "password" not in body:
+        app.logger.error("Request body is not JSON")
         abort(400, description="Request body must be correctly-shaped JSON!")
 
+    app.logger.debug("Checking user is authorized..")
     user = models.User.query.filter_by(username=body["username"]).first()
     if user is None or user.deactivated or not user.check_password(body["password"]):
+        app.logger.error("Unauthorized user")
         abort(401, description="Unauthorized")
 
     # get/update last login
@@ -59,9 +65,12 @@ def login():
         user.last_login = datetime.now()
         db.session.commit()
     except:
-        app.logger.warning("Failed to updated last_login for %s", user.username)
+        app.logger.warning("Failed to updated last_login for '%s'", user.username)
 
     login_user(user)
+    app.logger.info(
+        "User '%s' has successfully logged in, returning JSON.", user.username
+    )
     return jsonify(
         {
             "username": user.username,
@@ -76,6 +85,7 @@ def login():
 @login_required
 def logout():
     if not request.json:
+        app.logger.error("Logout failed as request body is not JSON")
         abort(400, description="Request body must be JSON!")
     logout_user()
     return "", 204
@@ -95,35 +105,37 @@ def validate_user(request_user: dict):
 @routes.route("/api/pipelines", methods=["GET"], endpoint="pipelines_list")
 @login_required
 def pipelines_list():
+    app.logger.info("Retrieving all pipelines..")
     db_pipelines = (
         db.session.query(models.Pipeline)
         .options(joinedload(models.Pipeline.supported))
         .all()
     )
-
+    app.logger.info("Returning pipelines as JSON..")
     return jsonify(db_pipelines)
 
 
 @routes.route("/api/institutions", methods=["GET"])
 @login_required
 def get_institutions():
-
+    app.logger.info("Retrieving all institutions..")
     db_institutions = models.Institution.query.all()
-
+    app.logger.info("Returning institutions as JSON..")
     return jsonify([x.institution for x in db_institutions])
 
 
 @routes.route("/api/metadatasettypes", methods=["GET"])
 @login_required
 def get_metadataset_types():
-
+    app.logger.info("Retrieving all metadataset types..")
     metadataset_dataset_types = models.MetaDatasetType_DatasetType.query.all()
-
+    app.logger.debug("Creating a dictionary of unique metadataset types")
     d = {e.metadataset_type: [] for e in metadataset_dataset_types}
-
+    app.logger.debug("Appending dataset types to each unique metadataset type")
     for k in metadataset_dataset_types:
+        app.logger.debug("Appending %s to %s", k.dataset_type, k.metadataset_type)
         d[k.metadataset_type].append(k.dataset_type)
-
+    app.logger.info("Returning JSON..")
     return jsonify(d)
 
 
@@ -131,9 +143,13 @@ def get_metadataset_types():
 @login_required
 def get_enums():
     enums = {}
+    app.logger.info("Retrieving all enums..")
     for name, obj in inspect.getmembers(models, inspect.isclass):
         if issubclass(obj, Enum) and name != "Enum":
+            app.logger.debug("%s is an enum", name)
             enums[name] = [e.value for e in getattr(models, name)]
+        else:
+            app.logger.debug("%s is NOT an enum", name)
         # cheat to also return the DatasetType and MetaDatasetType
         if name == "DatasetType":
             enums[name] = [
@@ -144,14 +160,14 @@ def get_enums():
                 e.metadataset_type
                 for e in db.session.query(getattr(models, name)).all()
             ]
-
+    app.logger.info("Returning JSON..")
     return jsonify(enums)
 
 
 @routes.route("/api/_bulk", methods=["POST"])
 @login_required
 def bulk_update():
-
+    app.logger.info("Starting bulk upload..")
     dataset_ids = []
 
     editable_dict = {
@@ -187,48 +203,69 @@ def bulk_update():
             "notes",
         ],
     }
-
+    app.logger.info("Checking content type..")
     if request.content_type == "text/csv":
+        app.logger.debug("Content type is csv, using pandas to read in.")
         try:
+            app.logger.debug("Reading in csv and converted to a dictionary..")
             dat = pd.read_csv(StringIO(request.data.decode("utf-8")))
             dat = dat.where(pd.notnull(dat), None)
             dat = dat.to_dict(orient="records")
         except Exception as err:
+            app.logger.error(
+                "CSV failed to be read in. Please verify the file is a properly formatted csv."
+            )
             abort(400, description=str(err))
 
     elif request.content_type == "application/json":
+        app.logger.debug("Content type is JSON..")
         if not request.json:
+            app.logger.debug("Content type is JSON, but request body isn't JSON..")
             abort(415, description="Request body must be JSON")
 
         # the jsons must be in a list, even if it is a single json object
         if not isinstance(request.json, list):
+            app.logger.debug(
+                "Content type is JSON and request body is JSON, but JSON isn't in an array.."
+            )
             abort(422, description="JSON must be in an array")
 
         dat = request.json
 
     else:
+        app.logger.error("Content type is not 'text/csv' or 'application/json'")
         abort(
             415,
             description="Only Content Type 'text/csv' or 'application/json' Supported",
         )
 
     try:
+        app.logger.debug(
+            "Assigning updated and created by IDs to user ID '%s'", current_user.user_id
+        )
         updated_by_id = current_user.user_id
         created_by_id = current_user.user_id
     except:  # LOGIN_DISABLED
+        app.logger.debug(
+            "LOGIN_DISABLED = True so updated and created by IDs are defaulting to 1."
+        )
         updated_by_id = 1
         created_by_id = 1
 
     if app.config.get("LOGIN_DISABLED") or current_user.is_admin:
         user_id = request.args.get("user", models.User.user_id)
+        app.logger.debug("User is admin with ID '%s'", user_id)
     else:
         user_id = current_user.user_id
+        app.logger.debug("User is regular with ID '%s'", user_id)
 
     # get user's group(s)
+    app.logger.info("Retrieving supplied user groups..")
     requested_groups = request.args.get("groups")
 
     if requested_groups:
         requested_groups = requested_groups.split(",")
+        app.logger.debug("User's groups are %s", requested_groups)
         groups = (
             models.Group.query.join(models.Group.users)
             .filter(
@@ -238,68 +275,120 @@ def bulk_update():
             .all()
         )
         if len(requested_groups) != len(groups):
+            app.logger.debug(
+                "User supplied groups and permission groups do not match up."
+            )
             abort(404, description="Invalid group code provided")
     else:
+        app.logger.debug("User did not provide groups, checking permissions..")
         groups = (
             models.Group.query.join(models.Group.users)
             .filter(models.User.user_id == user_id)
             .all()
         )
+        app.logger.debug("User is part of '%s'", [x.group_name for x in groups])
         if len(groups) != 1:
+            app.logger.debug(
+                "User belongs to multiple permission groups but no group was specified. User needs to specifiy group(s) they would like to add data in."
+            )
             abort(
                 400,
                 description="User belongs to multiple permission groups but no group was specified",
             )
         if not groups:
+            app.logger.error(
+                "Verify user '%s'; '%s' was added to any permission groups",
+                current_user.username,
+                current_user.user_id,
+            )
             abort(403, description="User does not belong to any permission groups")
 
+    app.logger.info("Begin processing and inserting records into the database..")
     for i, row in enumerate(dat):
+        app.logger.info("Start Record: %s", i)
+        app.logger.debug("\tChecking sequencing date is provided")
         sequencing_date = row.get("sequencing_date")
         if not sequencing_date:
+            app.logger.debug("\tNo sequencing date provided.")
             abort(400, description="A sequencing date must be provided")
+        else:
+            app.logger.debug("\tSequencing date provided: '%s'", sequencing_date)
 
         # Find the family by codename or create it if it doesn't exist
+
+        app.logger.debug(
+            "\tChecking whether family already exists through codename '%s'..",
+            row.get("family_codename"),
+        )
         family_id = models.Family.query.filter(
             models.Family.family_codename == row.get("family_codename")
         ).value("family_id")
         if not family_id:
+            app.logger.debug(
+                "\tFamily '%s' does not yet exist.., creating",
+                row.get("family_codename"),
+            )
             family = models.Family(
                 family_codename=row.get("family_codename"),
                 created_by_id=created_by_id,
                 updated_by_id=updated_by_id,
             )
             db.session.add(family)
+            app.logger.debug("\tInserting instance into database..")
             transaction_or_abort(db.session.flush)
             family_id = family.family_id
+        else:
+            app.logger.debug(
+                "\tFamily ID %s already exists and won't be created.", family_id
+            )
 
         # Fail if we have any invalid values
+        app.logger.debug("\tValidating enums for participants..")
         enum_error = enum_validate(
             models.Participant, row, editable_dict["participant"]
         )
         if enum_error:
+            app.logger.error("\tEnum invalid: " + enum_error)
             db.session.rollback()
             abort(400, description=f"Error on line {str(i + 1)} - " + enum_error)
+        else:
+            app.logger.debug("\tAll enums supplied are valid.")
 
         # get institution id
+        app.logger.debug("\tRetrieving institution")
         institution = row.get("institution")
         if institution:
+            app.logger.debug(
+                "\tChecking supplied institution name, '%s', exists..", institution
+            )
             institution_obj = models.Institution.query.filter(
                 models.Institution.institution == institution
             ).one_or_none()
             if institution_obj:
+                app.logger.debug("\tInstitution name exists, assigning its id")
                 institution_id = institution_obj.institution_id
             else:
+                app.logger.debug(
+                    "\tInstitution name does not exist, creating a new entry in the database"
+                )
                 institution_obj = models.Institution(institution=institution)
                 db.session.add(institution_obj)
                 transaction_or_abort(db.session.commit)
                 institution_id = institution_obj.institution_id
+        else:
+            app.logger.debug("\tNo institution supplied")
 
         # Find the participant by codename or create it if it doesn't exist
+        app.logger.debug(
+            "\tChecking participant exists through codename '%s'",
+            row.get("participant_codename"),
+        )
         participant_id = models.Participant.query.filter(
             models.Participant.family_id == family_id,
             models.Participant.participant_codename == row.get("participant_codename"),
         ).value("participant_id")
         if not participant_id:
+            app.logger.debug("\tParticipant does not exist, creating")
             participant = models.Participant(
                 family_id=family_id,
                 participant_codename=row.get("participant_codename"),
@@ -313,19 +402,28 @@ def bulk_update():
                 created_by_id=created_by_id,
                 updated_by_id=updated_by_id,
             )
+            app.logger.debug("\tCreating entry in the database..")
             db.session.add(participant)
             transaction_or_abort(db.session.flush)
             participant_id = participant.participant_id
+        else:
+            app.logger.debug(
+                "\tParticipant already exists and will not be added to the database"
+            )
 
         # Fail if we have any invalid values
+        app.logger.debug("\tValidating enums for tissue samples..")
         enum_error = enum_validate(
             models.TissueSample, row, editable_dict["tissue_sample"]
         )
         if enum_error:
             db.session.rollback()
             abort(400, description=f"Error on line {str(i + 1)}: " + enum_error)
+        else:
+            app.logger.debug("\tAll enums supplied are valid.")
 
         # Create a new tissue sample under this participant
+        app.logger.debug("\tCreating a new tissue sample..")
         tissue_sample = models.TissueSample(
             participant_id=participant_id,
             tissue_sample_type=row.get("tissue_sample_type"),
@@ -333,16 +431,22 @@ def bulk_update():
             created_by_id=created_by_id,
             updated_by_id=updated_by_id,
         )
+        app.logger.debug("\tCreating tissue sample entry in the database..")
         db.session.add(tissue_sample)
         transaction_or_abort(db.session.flush)
-
+        app.logger.debug("\tDone")
         # Fail if we have any invalid values
+        app.logger.debug("\tValidating enums for datasets..")
         enum_error = enum_validate(models.Dataset, row, editable_dict["dataset"])
         if enum_error:
+            app.logger.error("\tEnum invalid: " + enum_error)
             db.session.rollback()
             abort(400, description=f"Error on line {str(i + 1)} - " + enum_error)
+        else:
+            app.logger.debug("\tAll enums supplied are valid.")
 
         # Create a new dataset under the new tissue sample
+        app.logger.debug("\tCreating a new dataset..")
         dataset = models.Dataset(
             tissue_sample_id=tissue_sample.tissue_sample_id,
             dataset_type=row.get("dataset_type"),
@@ -358,20 +462,35 @@ def bulk_update():
             sequencing_date=row.get("sequencing_date"),
             batch_id=row.get("batch_id"),
         )
+        app.logger.debug("\tLinking files to dataset..")
         if request.content_type == "text/csv":
+            app.logger.debug(
+                "\tContent type is `text/csv` and linked files are expected to be | separated: '%s'",
+                row.get("linked_files", "").split("|"),
+            )
             files = row.get("linked_files", "").split("|")
         else:
+            app.logger.debug(
+                "\tContent type is NOT `test/csv` and linked files are expected to be in a list: '%s'",
+                row.get("linked_files", []),
+            )
             files = row.get("linked_files", [])
         dataset.files += [models.DatasetFile(path=path) for path in files if path]
 
+        app.logger.debug("\tAdding users groups to the dataset")
         dataset.groups += groups
-
+        app.logger.debug("\tCreating dataset entry in the database..")
         db.session.add(dataset)
         transaction_or_abort(db.session.flush)
+        app.logger.debug("\tDone")
         dataset_ids.append(dataset.dataset_id)
 
-    transaction_or_abort(db.session.commit)
+        app.logger.info("End Record: %s", i)
 
+    transaction_or_abort(db.session.commit)
+    app.logger.debug("%s datasets added", len(dataset_ids))
+
+    app.logger.debug("Fetching all created datasets..")
     db_datasets = (
         db.session.query(models.Dataset)
         .options(
@@ -398,5 +517,5 @@ def bulk_update():
         }
         for dataset in db_datasets
     ]
-
+    app.logger.info("Done, returning JSON..")
     return jsonify(datasets)
