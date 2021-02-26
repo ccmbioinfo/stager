80d98626dbe3b52569f6154ef1eb50babeaf3276
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2021-02-26 18:04:35 -0500

    Flask: implement progressive loading and filtering on /api/datasets (#389)
    * Default to joinedloads for user relationships
    
    * Create `paged` decorator for page and limit parameters
    
    * Implement paged loading and queries for /api/datasets, fix too many SQL queries performed by the endpoint
    
    * Add `filter_updated_or_abort` decorator

--- flask/app/models.py
+++ flask/app/models.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
-from datetime import datetime, date
+from datetime import date, datetime
 from enum import Enum
 from typing import List
+
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy import CheckConstraint
+from werkzeug.security import check_password_hash, generate_password_hash
 
 from . import db
-
 
 users_groups_table = db.Table(
     "users_groups",
@@ -75,8 +75,8 @@ class Family(db.Model):
     )
     participants = db.relationship("Participant", backref="family")
 
-    updated_by = db.relationship("User", foreign_keys=[updated_by_id])
-    created_by = db.relationship("User", foreign_keys=[created_by_id])
+    updated_by = db.relationship("User", foreign_keys=[updated_by_id], lazy="joined")
+    created_by = db.relationship("User", foreign_keys=[created_by_id], lazy="joined")
 
 
 class Sex(str, Enum):
@@ -280,8 +280,8 @@ class Dataset(db.Model):
     groups = db.relationship(
         "Group", secondary=groups_datasets_table, backref="datasets"
     )
-    updated_by = db.relationship("User", foreign_keys=[updated_by_id])
-    created_by = db.relationship("User", foreign_keys=[created_by_id])
+    updated_by = db.relationship("User", foreign_keys=[updated_by_id], lazy="joined")
+    created_by = db.relationship("User", foreign_keys=[created_by_id], lazy="joined")
 
     files = db.relationship(
         "DatasetFile",
@@ -361,9 +361,9 @@ class Analysis(db.Model):
     updated_by_id = db.Column(
         db.Integer, db.ForeignKey("user.user_id", onupdate="cascade"), nullable=False
     )
-    updated_by = db.relationship("User", foreign_keys=[updated_by_id])
-    assignee = db.relationship("User", foreign_keys=[assignee_id])
-    requester = db.relationship("User", foreign_keys=[requester_id])
+    updated_by = db.relationship("User", foreign_keys=[updated_by_id], lazy="joined")
+    assignee = db.relationship("User", foreign_keys=[assignee_id], lazy="joined")
+    requester = db.relationship("User", foreign_keys=[requester_id], lazy="joined")
 
 
 @dataclass
