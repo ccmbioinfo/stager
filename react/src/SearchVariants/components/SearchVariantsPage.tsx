5b225f932f7db5728ea54fa68a50ccbeb7b75482
Conor Klamann <conorklamann@gmail.com>
2021-04-22 14:03:13 -0400

    React: hook up variant viewer to variant-wise CSV endpoint (#518)
    Support multiple genes with autocomplete and hook up to backend
    Automatically download CSV on submit, handle 4xx and 5xx errors
    #483

--- react/src/SearchVariants/components/SearchVariantsPage.tsx
+++ react/src/SearchVariants/components/SearchVariantsPage.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { Box, Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import { Box, Button, Chip, Container, Grid, makeStyles, Typography } from "@material-ui/core";
+import { useSnackbar } from "notistack";
 import GeneAutocomplete from "./Autocomplete";
-import { Gene } from "../../typings";
-//import { useVariantsQuery } from "../../hooks/variants";
+import { useVariantsQuery } from "../../hooks/variants";
+import { downloadCsvResponse } from "../../hooks/utils";
 
 interface SearchVariantsPageProps {}
 
@@ -20,12 +21,48 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const SearchVariantsPage: React.FC<SearchVariantsPageProps> = () => {
-    const [selectedGene, setSelectedGene] = useState<Gene>();
+    const [selectedGenes, setSelectedGenes] = useState<string[]>([]);
+    const [error, setError] = useState(false);
+    const [csvFetchEnabled, setCsvFetchEnabled] = useState(false);
 
-    console.log(selectedGene);
-    // currently a dummy endpoint
-    /* eslint-disable @typescript-eslint/no-unused-vars */
-    //const { data: variants } = useVariantsQuery({ search: selectedGene }, "csv", !!selectedGene);
+    const { enqueueSnackbar } = useSnackbar();
+
+    const updateSelectedGenes = (gene: string) => {
+        if (gene && !selectedGenes.includes(gene)) {
+            setSelectedGenes(selectedGenes.concat(gene));
+        } else {
+            setSelectedGenes(selectedGenes.filter(g => g !== gene));
+        }
+    };
+
+    const { data: blob } = useVariantsQuery({ panel: selectedGenes.join(";") }, "csv", {
+        enabled: csvFetchEnabled,
+        onError: response => {
+            if (response.status === 400) {
+                setError(true);
+            } else {
+                const errorText = response.statusText;
+                enqueueSnackbar(`Query Failed. Error: ${errorText}`, { variant: "error" });
+            }
+        },
+    });
+
+    const clearError = () => {
+        if (error) {
+            setError(false);
+        }
+    };
+
+    useEffect(() => {
+        /* react-query cache will return a 'new' blob when parameters
+           match a cache key, even if refetch hasn't been called or query is disabled,
+           triggering a download. So we need to explicitly
+           enable/disable the query AND provide flag as a dependency. */
+        if (blob && csvFetchEnabled) {
+            downloadCsvResponse(blob);
+            setCsvFetchEnabled(false);
+        }
+    }, [blob, csvFetchEnabled]);
 
     useEffect(() => {
         document.title = `Search Variants | ${process.env.REACT_APP_NAME}`;
@@ -37,19 +74,72 @@ const SearchVariantsPage: React.FC<SearchVariantsPageProps> = () => {
         <main className={classes.content}>
             <div className={classes.appBarSpacer} />
             <Container className={classes.container} maxWidth={false}>
-                <Grid container justify="center">
+                <Grid spacing={3} direction="column" container alignItems="center">
                     <Grid item xs={12}>
                         <Typography align="center" variant="h3">
                             Search for Variants by Gene
                         </Typography>
                     </Grid>
-                    <Grid item xs={12} md={6}>
-                        <Box padding={4}>
+                    <Grid
+                        container
+                        justify="center"
+                        alignItems="center"
+                        spacing={1}
+                        item
+                        xs={12}
+                        md={6}
+                        wrap="nowrap"
+                    >
+                        <Grid item xs={12}>
                             <GeneAutocomplete
                                 fullWidth={true}
-                                onSelect={gene => setSelectedGene(gene)}
+                                onSearch={clearError}
+                                onSelect={gene =>
+                                    gene.hgnc_gene_name
+                                        ? updateSelectedGenes(gene.hgnc_gene_name)
+                                        : /* this shouldn't happen since we're querying on this field (for now) */
+                                          console.error(`Gene id ${gene.gene_id} has no gene name`)
+                                }
                             />
-                        </Box>
+                        </Grid>
+                        <Grid item>
+                            <Button
+                                disabled={!selectedGenes.length}
+                                onClick={() => setCsvFetchEnabled(true)}
+                                size="large"
+                                variant="contained"
+                            >
+                                Submit
+                            </Button>
+                        </Grid>
+                    </Grid>
+                    <Grid container item xs={12} md={6} wrap="nowrap">
+                        <Grid item>
+                            {!!selectedGenes.length && (
+                                <Box padding={1} margin={1}>
+                                    <Typography>Selected Genes</Typography>
+                                </Box>
+                            )}
+                        </Grid>
+                        <Grid item container xs={12}>
+                            {selectedGenes.map(g => (
+                                <Grid item key={g}>
+                                    <Box key={g} padding={1} margin={1}>
+                                        <Chip label={g} onDelete={() => updateSelectedGenes(g)} />
+                                    </Box>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                        {error && (
+                            <Typography align="center" color="error">
+                                No variants found for{" "}
+                                {selectedGenes.length === 1
+                                    ? selectedGenes[0]
+                                    : selectedGenes.join(", ")}
+                            </Typography>
+                        )}
                     </Grid>
                 </Grid>
             </Container>
