9a477531b3bbe7c2d8d5989fb686b0b46835f82d
Delvin So <so.delvin@gmail.com>
2021-04-26 17:01:03 -0400

    Flask: add GET /api/summary/participants (#525)
    Closes #482

--- flask/tests/test_variants.py
+++ flask/tests/test_variants.py
@@ -70,9 +70,88 @@ def test_get_variant_summary(test_database, client, login_as):
     assert response.status_code == 406
 
     login_as("user")
-    assert (
-        client.get(
-            "api/summary/variants?panel=LOXL4", headers={"Accept": "application/json"}
-        ).status_code
-        == 200
+    response = client.get(
+        "api/summary/variants?panel=LOXL4", headers={"Accept": "application/json"}
     )
+    assert response.status_code == 200
+    assert len(response.get_json()) == 1
+
+
+def test_get_participant_summary(test_database, client, login_as):
+
+    login_as("admin")
+
+    # single gene
+    response = client.get(
+        "api/summary/participants?panel=LOXL4",
+        headers={"Accept": "application/json"},
+    )
+
+    assert response.status_code == 200
+    # of participants
+    assert len(response.get_json()) == 2
+    # of variants for LOXL4 for each participant's dataset
+    for ptp in response.get_json():
+        variants = ptp.get("dataset").get("variants")
+        assert len(variants) == 3
+
+    # multiple genes
+    response = client.get(
+        "api/summary/participants?panel=LOXL4;RTEL1",
+        headers={"Accept": "application/json"},
+    )
+    assert response.status_code == 200
+    # # of participants
+    assert len(response.get_json()) == 2
+    # of variants for LOXL4 and RTEL1, for each participant's dataset
+    for ptp in response.get_json():
+        variants = ptp.get("dataset").get("variants")
+        assert len(variants) == 6
+
+    # invalid - no gene found
+    response = client.get(
+        "api/summary/participants?panel=BADGENENAME",
+        headers={"Accept": "application/json"},
+    )
+    assert response.status_code == 400
+
+    # not all genes found
+
+    response = client.get(
+        "api/summary/participants?panel=LOXL4;TYPODGENENAME",
+        headers={"Accept": "application/json"},
+    )
+    assert response.status_code == 400
+
+    # --- csv ----
+    response = client.get(
+        "api/summary/participants?panel=LOXL4", headers={"Accept": "text/csv"}
+    )
+
+    df = pd.read_csv(BytesIO(response.get_data()), encoding="utf8")
+    # the number of variants for the given participants in LOXL4 that have sufficient depth for zygosity (5)
+    assert df.shape[0] == 5
+
+    response = client.get(
+        "api/summary/participants?panel=LOXL4;RTEL1", headers={"Accept": "text/csv"}
+    )
+    # the number of variants for the given participants in LOXL4 AND RTEL1 that have sufficient depth for zygosity (4 + 5)
+    df = pd.read_csv(BytesIO(response.get_data()), encoding="utf8")
+
+    assert df.shape[0] == 9
+
+    # --- invalid accept header ----
+
+    response = client.get(
+        "api/summary/participants?panel=LOXL4",
+        headers={"Accept": "BAD_ACCEPT/HEADER"},
+    )
+    assert response.status_code == 406
+
+    login_as("user")
+    response = client.get(
+        "api/summary/participants?panel=LOXL4",
+        headers={"Accept": "application/json"},
+    )
+    assert response.status_code == 200
+    assert len(response.get_json()) == 2
