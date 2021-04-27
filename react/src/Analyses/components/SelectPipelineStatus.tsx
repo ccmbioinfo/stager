2b5363a5999ac888444ea626d47cda062f36b249
Mackenzie <11793063+frewmack@users.noreply.github.com>
2021-04-27 16:01:34 -0400

    Fix select side effect bug - Use initial value from props in status change check - Fix react fragment in select list

--- react/src/Analyses/components/SelectPipelineStatus.tsx
+++ react/src/Analyses/components/SelectPipelineStatus.tsx
@@ -1,5 +1,5 @@
-import React, { useMemo } from "react";
-import { MenuItem, Select, ListSubheader } from "@material-ui/core";
+import React, { useMemo, useState } from "react";
+import { MenuItem, Select, ListSubheader, SelectProps } from "@material-ui/core";
 import { EditComponentProps } from "material-table";
 import { Analysis, PipelineStatus } from "../../typings";
 import { checkPipelineStatusChange } from "../../functions";
@@ -7,6 +7,7 @@ import { useUserContext } from "../../contexts";
 
 export default function SelectPipelineStatus(props: EditComponentProps<Analysis>) {
     const userClient = useUserContext();
+    const [oldValue] = useState<PipelineStatus>(props.value);
     const options = useMemo(() => {
         if (userClient.user.is_admin) {
             const [valid, invalid] = Object.values(PipelineStatus).reduce(
@@ -19,29 +20,33 @@ export default function SelectPipelineStatus(props: EditComponentProps<Analysis>
                         return prev;
                     }
                 },
-                [[], []] as string[][]
+                [[], []] as PipelineStatus[][]
             );
 
-            return (
-                <>
-                    <ListSubheader>Valid States</ListSubheader>
-                    {valid.map(value => (
-                        <MenuItem value={value}>{value}</MenuItem>
-                    ))}
-                    <ListSubheader>Invalid States</ListSubheader>
-                    {invalid.map(value => (
-                        <MenuItem value={value}>{value}</MenuItem>
-                    ))}
-                </>
-            );
+            return [
+                <ListSubheader>Valid States</ListSubheader>,
+                valid.map(value => <MenuItem value={value}>{value}</MenuItem>),
+                <ListSubheader>Invalid States</ListSubheader>,
+                invalid.map(value => <MenuItem value={value}>{value}</MenuItem>),
+            ].flat();
         }
         return Object.values(PipelineStatus)
-            .filter(state => props.value === state || checkPipelineStatusChange(props.value, state))
+            .filter(
+                state =>
+                    props.value === state ||
+                    state === oldValue ||
+                    checkPipelineStatusChange(oldValue, state)
+            )
             .map(value => <MenuItem value={value}>{value}</MenuItem>);
-    }, [props.value, userClient]);
+    }, [props.value, oldValue, userClient.user.is_admin]);
+
+    const handleChange: SelectProps["onChange"] = e => {
+        const newValue = e.target.value as string;
+        props.onChange(newValue);
+    };
 
     return (
-        <Select value={props.value} onChange={e => props.onChange(e.target.value)}>
+        <Select value={props.value} onChange={handleChange}>
             {options}
         </Select>
     );
