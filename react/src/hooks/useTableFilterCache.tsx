c36b5f5df0be465e5985fad98f7ebc21a665af84
Mackenzie <11793063+frewmack@users.noreply.github.com>
2021-05-12 14:39:50 -0400

    Use shorthand notation

--- react/src/hooks/useTableFilterCache.tsx
+++ react/src/hooks/useTableFilterCache.tsx
@@ -42,7 +42,7 @@ export function useTableFilterCache<RowData extends object>(cacheKey: string) {
     };
 
     return {
-        handleFilterChange: handleFilterChange,
-        setInitialFilters: setInitialFilters,
+        handleFilterChange,
+        setInitialFilters,
     };
 }
