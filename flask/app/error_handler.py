41221cb0a1cd256f72040e2a6edd2aabb5ec2037
Delvin So <so.delvin@gmail.com>
2021-07-13 14:34:56 -0400

    changes to log only exceptions in the 500s

--- flask/app/error_handler.py
+++ flask/app/error_handler.py
@@ -7,10 +7,22 @@ error_blueprint = Blueprint("error_handler", __name__)
 
 @error_blueprint.app_errorhandler(Exception)
 def handle_error(error: Exception):
+
+    werkzeug_500_codes = [
+        500,  # internal error
+        501,  # not implemented
+        502,  # bad gateway
+        503,  # service unavailable
+        504,  # gateway timeout
+        505,  # http ver. not supported
+    ]
     code = 500  # defaults to 500 for non HTTP exceptions
     msg = error
+
     if isinstance(error, HTTPException):
         code = error.code
         msg = error.description
-        app.logger.error(traceback.format_exc())
+        # 500 codes are handled as they all exist as a subclass of HTTPException
+        if code in werkzeug_500_codes:
+            app.logger.error(traceback.format_exc())
     return jsonify(error=str(msg)), code
