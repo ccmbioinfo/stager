dfab546b004b6bea9626712547cb2b4425cd67a8
Hannah Le <hannah.lgbhan@gmail.com>
2021-07-20 19:17:21 -0400

    - Changed order of error message

--- react/src/hooks/useErrorSnackbar.tsx
+++ react/src/hooks/useErrorSnackbar.tsx
@@ -3,14 +3,14 @@ import { useSnackbar } from "notistack";
 /**
  * Hook that returns a function for opening an error snackbar that displays an error code and an error message.
  *
- * @returns function that takes in the error response and an optional error message 
+ * @returns function that takes in the error response and an optional error message
  */
 
 export function useErrorSnackbar() {
     const { enqueueSnackbar } = useSnackbar();
 
     const enqueueErrorSnackbar = (response: Response, message?: string | undefined) => {
-        enqueueSnackbar(`${message}. Error: ${response.status} - ${response.statusText}`, {
+        enqueueSnackbar(`Error: ${response.status} - ${response.statusText}. ${message}`, {
             variant: "error",
         });
     };
