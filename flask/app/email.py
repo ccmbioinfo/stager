6b94b6407dfb678f1c4f4c6477b0940913fd3b55
Hannah Le <hannah.lgbhan@gmail.com>
2021-11-12 20:31:51 -0500

    Working email scheduler

--- flask/app/email.py
+++ flask/app/email.py
@@ -2,49 +2,54 @@
 # https://github.com/sendgrid/sendgrid-python
 import os
 import json
-from datetime import date, datetime, timedelta
+from datetime import datetime, timedelta
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail, SendAt
-from flask import jsonify, current_app as app
+from sendgrid.helpers.mail import Mail, SendAt, To, From, Subject, Content, MimeType
+from flask import current_app as app
+from pytz import timezone
+import math
 
 
 sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
 
+tz = timezone("EST")
+
 
 def send_email(from_email, to_emails, subject, content):
-    message = Mail(
-        from_email=from_email,
-        to_emails=to_emails,
-        subject=subject,
-        html_content=content,
-    )
 
     emails_stats = get_daily_stats()
 
-    message.send_at = SendAt(get_send_time(emails_stats))
+    scheduled_time = get_send_time(emails_stats)
+
+    message = Mail()
+
+    message.to = To(to_emails)
+    message.from_email = From(from_email)
+    message.subject = Subject(subject)
+    message.content = Content(MimeType.text, content)
+    message.send_at = SendAt(math.ceil(scheduled_time))
 
     try:
         sg.send(message)
-        app.logger.info(f"Email successfully sent from {from_email} to {to_emails}...")
+        print(
+            f"Email successfully sent from {from_email} to {to_emails} at {datetime.fromtimestamp(scheduled_time, tz)}"
+        )
     except Exception as e:
         app.logger.error("Failed to send email...")
         app.logger.error(e)
 
 
 def get_daily_stats():
-    today = date.today().strftime("%Y-%m-%d")
-    future = date.max.strftime("%Y-%m-%d")
+    today = datetime.now(tz).strftime("%Y-%m-%d")
     params = {
         "aggregated_by": "day",
-        "limit": 1,
         "start_date": today,
-        "end_date": future,
+        "end_date": today,
         "offset": 1,
     }
     try:
         response = sg.client.stats.get(query_params=params)
-        print("body", response.body.decode("utf-8"))
-        return response.body.decode("utf-8")
+        return json.loads(response.body.decode("utf-8"))
     except:
         app.logger.error("Failed to get daily email stats")
         return []
@@ -52,15 +57,22 @@ def get_daily_stats():
 
 def get_send_time(stats):
     limit_per_day = 100
-    send_at = datetime.now()
-    yesterday = datetime.today() - timedelta(days=1)
+    send_at = datetime.now(tz)
 
     for stat in stats:
 
-        requests_count = sum([r.metrics.requests for r in r.stats])
-        date = stat.date.strptime(stat.date, "%Y-%m-%d")
+        requests_count = sum(
+            [r.get("metrics").get("requests") for r in stat.get("stats")]
+        )
 
-        if requests_count < limit_per_day and date < send_at and date > yesterday:
-            send_at = datetime.strptime(stat.date, "%Y-%m-%d")
+        if stat.get("date") == stringify_date(send_at):
+            if requests_count < limit_per_day:
+                send_at = send_at + timedelta(minutes=3)
+            else:
+                send_at = send_at + timedelta(days=1, minutes=3)
 
     return send_at.timestamp()
+
+
+def stringify_date(date):
+    return datetime.strftime(date, "%Y-%m-%d")
