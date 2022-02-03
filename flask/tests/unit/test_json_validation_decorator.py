d6e311c902346ad30f7b0f01e12704cf1011750c
Jenny <ziyixu686@gmail.com>
2022-02-03 11:20:53 -0500

    Flask: upgrade to python 3.8 (again) (#1042)
    This PR is a continuation of #1028, reverted #1031. In production, importing our main wsgi module raised an error because `schemas.py` tries to `import typing_extensions`. This is not a standard library, but a transitive dependency of black, only available in dev. Furthermore, that import wasn't actually used in the file.
    
    With this PR, simulating the production environment with `docker-compose up app_gunicorn` raised no errors.

--- flask/tests/unit/test_json_validation_decorator.py
+++ flask/tests/unit/test_json_validation_decorator.py
@@ -1,32 +1,32 @@
-""" test json validator """
-from unittest.mock import patch
-from unittest import TestCase
+from pytest import raises
 from werkzeug.exceptions import UnsupportedMediaType
 from app.utils import validate_json
 
 
-class ValidationTest(TestCase):
-    """test class for validation"""
+def test_validate_json_throws_415_on_non_json_header(application):
+    """test that validate_json throws when content-type is not json"""
 
-    @patch("app.utils.request")
-    def test_validate_json_throws_415_on_non_json_header(self, mock_request):
-        """test that validate_json throws when content-type is not json"""
-        mock_request.headers = {"Content-Type": "application/foo"}
+    @validate_json
+    def mock_route():
+        return True
 
-        @validate_json
-        def mock_route():
-            return True
+    with application.test_request_context(
+        "/api", headers={"Content-Type": "application/foo"}
+    ):
 
-        with self.assertRaises(UnsupportedMediaType):
+        with raises(UnsupportedMediaType):
             mock_route()
 
-    @patch("app.utils.request")
-    def test_validate_json_passes_on_json_header(self, mock_request):
-        """test that validate_json raises no exception when content-type is json"""
-        mock_request.headers = {"Content-Type": "application/json"}
 
-        @validate_json
-        def mock_route():
-            return True
+def test_validate_json_passes_on_json_header(application):
+    """test that validate_json raises no exception when content-type is json"""
+
+    @validate_json
+    def mock_route():
+        return True
+
+    with application.test_request_context(
+        "/api", headers={"Content-Type": "application/json"}
+    ):
 
         assert mock_route()
