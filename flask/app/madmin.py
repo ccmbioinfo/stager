ccmbioinfo/stager

commit 0e4ca8b2e8e20cc2cc3c061fa31e67540995d4d3

Flask: fix admins not having MinIO delete permissions (#1012)
Deny overrides Allow, so change the per-group policy to use explicit Allows. All policies: https://docs.min.io/minio/baremetal/security/minio-identity-management/policy-based-access-control.html

I will run `migrate-minio-policies` after merging.

flask/app/madmin.py
@@ -154,16 +154,18 @@ def stager_buckets_policy(*buckets: str) -> Dict[str, Any]:
         "Statement": [
             {
                 "Effect": "Allow",
-                "Action": ["s3:*"],
-                "Resource": arns,
-            },
-            {
-                "Effect": "Deny",
                 "Action": [
-                    "s3:DeleteBucket",
-                    "s3:ForceDeleteBucket",
-                    "s3:DeleteObject",
-                    "s3:DeleteObjectVersion",
+                    "s3:CreateBucket",
+                    "s3:GetBucketLocation",
+                    "s3:ListAllMyBuckets",
+                    "s3:GetObject",
+                    "s3:ListBucket",
+                    "s3:PutObject",
+                    "s3:PutObjectTagging",
+                    "s3:GetObjectTagging",
+                    "s3:AbortMultipartUpload",
+                    "s3:ListMultipartUploadParts",
+                    "s3:ListBucketMultipartUploads",
                 ],
                 "Resource": arns,
             },

commit c515a8ac38c6ba07913b57742161b5fb55a51e98

Flask: fix mc mirror --watch failing with Access Denied (#1041)
--watch seems to additionally depend on the Bucket Notifications API https://docs.min.io/minio/baremetal/security/minio-identity-management/policy-based-access-control.html

Fixes regression introduced in #1012

flask/app/madmin.py
@@ -167,6 +167,10 @@ def stager_buckets_policy(*buckets: str) -> Dict[str, Any]:
                     "s3:AbortMultipartUpload",
                     "s3:ListMultipartUploadParts",
                     "s3:ListBucketMultipartUploads",
+                    "s3:GetBucketNotification",
+                    "s3:PutBucketNotification",
+                    "s3:ListenNotification",
+                    "s3:ListenBucketNotification",
                 ],
                 "Resource": arns,
             },