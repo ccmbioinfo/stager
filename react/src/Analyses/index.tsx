0d529c07ec926af67280d153f39d9ef05ab512fc
Hannah Le <hannah.lgbhan@gmail.com>
2022-03-03 14:12:02 -0500

    Added refresh table for AnalysisInfoDialog

--- react/src/Analyses/index.tsx
+++ react/src/Analyses/index.tsx
@@ -433,6 +433,7 @@ export default function Analyses() {
                     onClose={() => {
                         setDetail(false);
                     }}
+                    refreshTable={tableRef.current.onQueryChange}
                 />
             )}
 
