14cc40b7519d022c0ba278fe3ad36fd9e6647b3b
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2022-03-21 11:45:02 -0400

    docs

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -106,9 +106,30 @@ def register_extensions(app):
 
 
 def config_logger(app):
+    """
+    Configure the main loggers: Flask application, SQLAlchemy, and werkzeug
+
+    SQLAlchemy logs can be very noisy and hard to filter out with grep because
+    the logged queries can span multiple lines. However, they are still useful
+    for auditing query efficiency and performance. Therefore, instead of using
+    basicConfig to configure the log handler and formatter at the root level,
+    which affects all loggers, the SQLAlchemy logger is configured separately
+    from the application logger.
+
+    Configuring handlers to write to a file instead of stdout violates Docker's
+    approach to logging. Instead, divert logs to a sidecar container so the
+    SQLAlchemy logs are separated from application logs. The application
+    container and the sidecar container both mount the same volume to share the
+    logs. This solution is adapted from a Kubernetes paradigm.
+
+    In production, SQLALCHEMY_SIDECAR specifies a writable filesystem path.
+    Rather than using a regular file that would increase the disk space required
+    for these logs and need to be rotated, use a FIFO or named pipe. This is
+    equivalent to `app | sidecar`, but we cannot use a regular pipe across
+    containers. This does not use disk space to redundantly store the logs, and
+    the sidecar can still output the logs the same way with `tail`.
+    """
     if app.config["SQLALCHEMY_LOG"]:
-        # Redirect noisy SQLAlchemy logs to a sidecar container through a FIFO (named pipe)
-        # if the environment variable is specified, typically in production
         sidecar = os.getenv("SQLALCHEMY_SIDECAR")
         if sidecar:
             try:
@@ -123,7 +144,7 @@ def config_logger(app):
             logging.getLogger("sqlalchemy").addHandler(handler)
         logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
         logging.getLogger("sqlalchemy.pool").setLevel(logging.INFO)
-
+    # This configures Flask's logger and then we can customize it after
     flask_logging.create_logger(app)
     "%(asctime)s in development?"
     flask_logging.default_handler.setFormatter(
