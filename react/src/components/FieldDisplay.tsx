b5ef6ca37e16d14b133e4d6b86bb4440e673561d
Jenny Xu <ziyixu686@gmail.com>
2022-04-08 14:02:26 -0400

    Add tooltip for dataset_type fields in modals

--- react/src/components/FieldDisplay.tsx
+++ react/src/components/FieldDisplay.tsx
@@ -1,18 +1,28 @@
-import React from "react";
-import { Typography } from "@material-ui/core";
-// import { formatFieldValue } from "../functions";
+import { Tooltip, Typography } from "@material-ui/core";
+import { useAPIInfoContext } from "../contexts";
 import { formatDisplayValue } from "../functions";
 import { Field } from "../typings";
-
 export interface FieldDisplayProps {
     field: Field;
 }
 
 /* Simple Typography component to display "title: value" */
 export default function FieldDisplay({ field }: FieldDisplayProps) {
+    const apiInfo = useAPIInfoContext() ?? undefined;
     return (
         <Typography variant="body1" gutterBottom>
-            <b>{field.title}:</b> <span>{formatDisplayValue(field)}</span>
+            <b>{field.title}:</b>{" "}
+            {field.title === "Dataset Type" && typeof field.value === "string" ? (
+                <Tooltip
+                    title={apiInfo?.dataset_types[field.value].name || " "}
+                    arrow
+                    placement="top"
+                >
+                    <span>{field.value}</span>
+                </Tooltip>
+            ) : (
+                <span>{formatDisplayValue(field)}</span>
+            )}
         </Typography>
     );
 }
