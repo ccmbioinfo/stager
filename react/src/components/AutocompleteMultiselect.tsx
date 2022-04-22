ccmbioinfo/stager

commit d4e5ca72753c2e72e80a5625e71d7148be8aaf8c

Flask: made unlinked file query non-recursive to reduce payload (#1029)

react/src/components/AutocompleteMultiselect.tsx
@@ -34,7 +34,8 @@ export default function AutocompleteMultiselect<T extends Record<string, any>>({
     noOptionsText,
 }: AutocompleteMultiselectProps<T>) {
     const [inputValue, setInputValue] = useState("");
-    const [removedOptions, setRemovedOptions] = useState<T[]>([]);
+
+    const isUnlinkedFileFolder = (path: string) => path.endsWith("/");
 
     return (
         <Autocomplete
@@ -53,18 +54,21 @@ export default function AutocompleteMultiselect<T extends Record<string, any>>({
             disableClearable={true}
             disableCloseOnSelect
             onChange={(event, selectedOptions, reason, details) => {
-                if (reason === "remove-option") {
-                    removedOptions.push(details!.option);
-                    setRemovedOptions(removedOptions);
-                } else if (reason === "select-option") {
-                    setRemovedOptions(removedOptions.filter(v => v.path !== details!.option.path));
-                }
-
                 //prevent keypress events from propagating to parent listeners
                 event.stopPropagation();
                 if (selectedOptions && ["select-option", "remove-option"].includes(reason)) {
-                    onSelect(selectedOptions);
-                    setInputValue("");
+                    const latestOption = selectedOptions[selectedOptions.length - 1];
+
+                    // If the option chosen is a folder, change the input value instead of selecting it as an option.
+                    if (latestOption && isUnlinkedFileFolder(latestOption.path)) {
+                        setInputValue(latestOption.path);
+                        if (onInputChange) {
+                            onInputChange(latestOption.path);
+                        }
+                    } else {
+                        onSelect(selectedOptions);
+                        setInputValue("");
+                    }
                 }
             }}
             renderOption={option => <span>{option[uniqueLabelPath]}</span>}
@@ -72,7 +76,7 @@ export default function AutocompleteMultiselect<T extends Record<string, any>>({
             getOptionSelected={(option, value) =>
                 option[uniqueLabelPath] === value[uniqueLabelPath]
             }
-            options={[...new Set(options.concat(removedOptions))]}
+            options={options}
             filterOptions={createFilterOptions({
                 limit,
                 stringify: o => o[uniqueLabelPath],

flask/app/buckets.py
@@ -62,7 +62,9 @@ def get_unlinked_files():
         # Get files with prefix in the bucket user specifies
         app.logger.debug("Getting all files in user minio buckets..")
         all_files = []
-        objs = minio_client.list_objects(bucket_name, prefix=file_name, recursive=True)
+        objs = minio_client.list_objects(bucket_name, prefix=file_name, recursive=False)
+
+        app.logger.debug("all files", objs)
 
         for obj in objs:
             if file_name in obj.object_name:

react/src/hooks/unlinked/useUnlinkedFilesQuery.tsx
@@ -20,7 +20,7 @@ export function useUnlinkedFilesQuery(
         ["unlinked", params],
         () => fetchFiles(params),
         {
-            staleTime: Infinity,
+            staleTime: 1000,
             retry: false,
             refetchInterval: false,
             refetchOnMount: false,

commit a9d61f32c473e4ed395c05f2c51680eb9d0e8ce6

React: fix candidate genes dropdown (#1122)
Fixes #1075

Regression seems to have been introduced in #1029 as the AutocompleteMultiselect component is also used by CandidateGeneCell

react/src/components/AutocompleteMultiselect.tsx
@@ -35,8 +35,6 @@ export default function AutocompleteMultiselect<T extends Record<string, any>>({
 }: AutocompleteMultiselectProps<T>) {
     const [inputValue, setInputValue] = useState("");
 
-    const isUnlinkedFileFolder = (path: string) => path.endsWith("/");
-
     return (
         <Autocomplete
             autoComplete
@@ -58,9 +56,8 @@ export default function AutocompleteMultiselect<T extends Record<string, any>>({
                 event.stopPropagation();
                 if (selectedOptions && ["select-option", "remove-option"].includes(reason)) {
                     const latestOption = selectedOptions[selectedOptions.length - 1];
-
                     // If the option chosen is a folder, change the input value instead of selecting it as an option.
-                    if (latestOption && isUnlinkedFileFolder(latestOption.path)) {
+                    if (latestOption.path?.endsWith("/")) {
                         setInputValue(latestOption.path);
                         if (onInputChange) {
                             onInputChange(latestOption.path);