cfc81badacb5da22628654a6540c5b8fa0121c26
Delvin So <22638338+delvinso@users.noreply.github.com>
2022-05-03 11:03:27 -0400

    Flask: set column types explicitly when reading in a csv for bulk upload (#1134)
    Fixes #1124

---
+++ flask/tests/unit/test_pandas_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+
+def test_csv_dtypes():
+    """for _bulk, to test a column of ints from a csv is correctly parsed as an object"""
+    df = pd.read_csv("tests/samplecsv2.csv")
+    assert df.dtypes["family_codename"] == np.int64
+    df2 = pd.read_csv(
+        "tests/samplecsv2.csv",
+        dtype={"family_codename": "string", "participant_codename": "string"},
+    )
+    assert df2.dtypes["family_codename"] == "string"
