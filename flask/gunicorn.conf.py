8bd2c7f395fd8a2264ff5d566d0654563de11382
Kevin Lu <6320810+kevinlul@users.noreply.github.com>
2022-06-08 13:12:06 -0400

    Move some configuration to gunicorn.conf.py as a crucial part of the application

--- flask/gunicorn.conf.py
+++ flask/gunicorn.conf.py
@@ -1,5 +1,20 @@
 from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
 
+from wsgi import app
+
+
+# --preload https://docs.gunicorn.org/en/stable/settings.html#preload-app
+# Initialize the app instance and threads in the master process. Workers are then
+# forked from the master process, which does not preserve any threads. This is
+# not specifically required to initialize a copy of the app instance in the master,
+# as the import above will create one regardless, but in the absence of the import
+# and the preload configuration, there would be no instance in the master.
+preload_app = True
+# --access-logfile - (stdout) https://docs.gunicorn.org/en/stable/settings.html#accesslog
+accesslog = "-"
+# --log-file - (stderr) https://docs.gunicorn.org/en/stable/settings.html#errorlog
+errorlog = "-"
+
 
 def when_ready(server):
     GunicornPrometheusMetrics.start_http_server_when_ready(8080)
